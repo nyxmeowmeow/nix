2fab981a08be0ecea4c12169cbf400ec09455049
rainmeower <rraaiin@proton.me>
2025-04-09 00:54:25 +1000

    a

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }: {
+
   imports = [
       ./hardware-configuration.nix
+      ./services.nix
     ];
 
   environment.systemPackages = with pkgs; [
@@ -62,22 +64,17 @@
 ];
 
 
-  networking.hostName = "nixos"; # Define your hostname.
 
   fonts.packages = with pkgs; [
     pkgs.nerd-fonts.mononoki
     pkgs.nerd-fonts.iosevka
   ];
 
-  # Enable networking
+  networking.hostName = "nixos"; # Define your hostname.
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Australia/Brisbane";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_AU.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_AU.UTF-8";
     LC_IDENTIFICATION = "en_AU.UTF-8";
@@ -110,34 +107,19 @@
   };
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  services.flatpak.enable = true;
 
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "amdgpu" ];
 
-  services.udisks2.enable = true;
-  systemd.services.udiskie.enable = true;
-  services.gvfs.enable = true;
-
-
-
-  services.dbus.enable = true;
-
-  
-  systemd.user.services.clipse = {
-    wantedBy = [ "default.target" ];
-    serviceConfig.ExecStart = "${pkgs.clipse}/bin/clipse";
-  };
 
 
 
 
-  services.xserver.displayManager = {
-    gdm.enable = false;
-    lightdm.enable = false;
-  };
+  
+
+
+
+
 
 
   security.polkit.enable = true;
@@ -151,58 +133,6 @@
     });
   '';
 
-  systemd = {
-  user.services.polkit-gnome-authentication-agent-1 = {
-    description = "polkit-gnome-authentication-agent-1";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-  };
-
-  
-
-
-
-  systemd.tmpfiles.rules = [
-    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
-  ];
-
-
-
-
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "au";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
---
+++ services.nix
@@ -0,0 +1,71 @@
+{ config, lib, pkgs, ... }: {
+
+  services.flatpak.enable = true;
+
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  services.xserver.displayManager = {
+    gdm.enable = false;
+    lightdm.enable = false;
+  };
+
+  services.udisks2.enable = true;
+  systemd.services.udiskie.enable = true;
+  services.gvfs.enable = true;
+
+  services.dbus.enable = true;
+
+  systemd.user.services.clipse = {
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.clipse}/bin/clipse";
+  };
+
+
+
+  systemd = {
+  user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
+
+  systemd.tmpfiles.rules = [
+    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+  ];
+
+
+
+  services.xserver.xkb = {
+    layout = "au";
+    variant = "";
+  };
+
+  services.printing.enable = true; # cups
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # jack.enable = true;
+
+    ## use the example session manager (no others are packaged yet so this is enabled by default,
+    ## no need to redefine it in your config for now)
+    # media-session.enable = true;
+  };
+
+
+}
