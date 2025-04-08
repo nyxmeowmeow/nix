048a4c35460fdd02266888ca0727ac62eeb6029e
rainmeower <rraaiin@proton.me>
2025-04-08 21:19:42 +1000

    2025/04/08 21:19:42

--- homeManagerModules/clipse.nix
+++ homeManagerModules/clipse.nix
@@ -4,5 +4,21 @@
     imageDisplay = {
       type = "sixel";
     };
+    theme = {
+      useCustomTheme = true;
+      DimmedDesc = "#a5adcb";
+      DimmedTitle = "#a5adcb";
+      FilteredMatch = "#cad3f5";
+      NormalDesc = "#000000";
+      NormalTitle = "#cad3f5";
+      SelectedDesc = "#ffffff";
+      SelectedTitle = "#ffffff";
+      SelectedBorder = "#ffffff";
+      SelectedDescBorder = "#ffffff";
+      TitleFore = "#ffffff";
+      Titleback = "#434C5E";
+      StatusMsg = "#ffffff";
+      PinIndicatorColor = "#ff0000";
+    };
   };
 }
