88151992cb4145352e0959db822e5da38796d31a
rainmeower <rraaiin@proton.me>
2025-04-08 21:27:38 +1000

    2025/04/08 21:27:38

--- homeManagerModules/hyprpaper.nix
+++ homeManagerModules/hyprpaper.nix
@@ -8,13 +8,13 @@
       splash_offset = 2.0;
 
       preload = [
-        "~Pictures/wallpapers/buildings1b.png" 
-        "~Pictures/wallpapers/buildings2b.png" 
+        "~/Pictures/wallpapers/buildings1b.png" 
+        "~/Pictures/wallpapers/buildings2b.png" 
       ];
 
       wallpaper = [
-        "DP-3,~Pictures/wallpapers/buildings1b.png"
-        "DP-1,~Pictures/wallpapers/buildings2b.png"
+        "DP-3,~/Pictures/wallpapers/buildings1b.png"
+        "DP-1,~/Pictures/wallpapers/buildings2b.png"
       ];
     };
   };
