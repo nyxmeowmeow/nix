5215f09329bb472a50af5fd7ba4fb6153883aa0e
rainmeower <rraaiin@proton.me>
2025-04-10 07:09:36 +1000

    10/04 07:09:36

--- homeManagerModules/hyprland.nix
+++ homeManagerModules/hyprland.nix
@@ -9,13 +9,13 @@
 
 
 
-#  home.pointerCursor = {
-#    gtk.enable = true;
-#    x11.enable = true;
-#    package = pkgs.bibata-cursors;
-#    name = "Bibata-Modern-Classic";
-#    size = 20;
-#  };
+ home.pointerCursor = {
+   gtk.enable = true;
+   x11.enable = true;
+   package = pkgs.bibata-cursors;
+   name = "Bibata-Modern-Classic";
+   size = 20;
+ };
 
  gtk = {
    enable = true;
@@ -142,8 +142,8 @@
         enabled = "false";
       };
       blur = {
-        enabled = "false";
-        special = "true";
+        enabled = "true";
+        special = "false";
         size = "6";
         passes = "3";
         new_optimizations = "on";
