763ece1ddb472fc61f9ec15d21656bc0bdda2a79
rainmeower <rraaiin@proton.me>
2025-04-10 23:16:20 +1000

    10/04 23:16:20

--- services.nix
+++ services.nix
@@ -61,10 +61,10 @@
     '';
   };
 
-  # systemd.services.mpd.environment = {
-  #   # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
-  #   XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.meow.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
-  # };
+  systemd.services.mpd.environment = {
+    # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
+    XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.meow.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
+  };
 
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
