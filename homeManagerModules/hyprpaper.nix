745f4bcd64321181a63a8607a9df96ec3833f1cd
rainmeower <rraaiin@proton.me>
2025-04-11 01:43:16 +1000

    11/04 01:43:16

--- homeManagerModules/hyprpaper.nix
+++ homeManagerModules/hyprpaper.nix
@@ -8,13 +8,13 @@
       splash_offset = 2.0;
 
       preload = [
-        "../stuff/1_hor.png" 
-        "../stuff/1_vert.png" 
+        "~/nix/stuff/1_hor.png" 
+        "~/nix/stuff/1_vert.png" 
       ];
 
       wallpaper = [
-        "DP-1,../stuff/1_hor.png"
-        "DP-3,../stuff/1_vert.png"
+        "DP-1,~/nix/stuff/1_hor.png"
+        "DP-3,~/nix/stuff/1_vert.png"
       ];
     };
   };
