7e111ad1cf7a0b7b075bc28d58c3765bf87dc9dd
rainmeower <rraaiin@proton.me>
2025-04-10 23:38:21 +1000

    10/04 23:38:21

--- homeManagerModules/foot.nix
+++ homeManagerModules/foot.nix
@@ -53,4 +53,52 @@
       };
     };
   };
+
+
+  home.file.".config/foot/float".text = ''
+    shell=fish
+    app-id=footblurmefloatme
+    title=footblurmefloatme
+    font=mononoki nerd font mono:size=20
+
+
+    [cursor]
+    style=beam
+    blink=yes
+    blink-rate=500
+    unfocused-style=unchanged
+    color=24273a cad3f5
+
+
+    [colors]
+    alpha=0
+    background=24273a
+    foreground=cad3f5
+    flash=7f7f00
+    flash-alpha=0.5
+
+    ## Normal/regular colors (color palette 0-7)
+    regular0=242424  # black
+    regular1=ed8796  # red
+    regular2=a6da95  # green
+    regular3=eed49f  # yellow
+    regular4=b7bdf8  # blue
+    regular5=c6a0f6  # magenta
+    regular6=8bd5ca  # cyan
+    regular7=e6e6e6  # white
+
+    ## Bright colors (color palette 8-15)
+    bright0=616161   # bright black
+    bright1=ed8796   # bright red
+    bright2=a6da95   # bright green
+    bright3=eed49f   # bright yellow
+    bright4=b7bdf8   # bright blue
+    bright5=c6a0f6   # bright magenta
+    bright6=8bd5ca   # bright cyan
+    bright7=ffffff   # bright white
+
+
+    [tweak]
+    box-drawing-base-thickness=0.06
+  '';
 }
--- services.nix
+++ services.nix
@@ -63,7 +63,7 @@
 
   systemd.services.mpd.environment = {
     # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
-    XDG_RUNTIME_DIR = "/run/user/1000"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
+    XDG_RUNTIME_DIR = "/run/user/1000"; # only worked when i manually specified 1000
   };
 
   services.pulseaudio = {
