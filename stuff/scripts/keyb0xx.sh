b9ab2f34938f268d275e71955b4258209e5cf9bc
rainmeower <rraaiin@proton.me>
2025-04-14 06:40:18 +1000

    14/04 06:40:18

--- stuff/scripts/keyb0xx.sh
+++ stuff/scripts/keyb0xx.sh
@@ -3,9 +3,14 @@
 
 spawn sudo /home/meow/misc/keyb0xx
 
+expect "password for meow:"
+send -- "nrts\r"
+
 expect "Enter a number (or 'a' to show all devices):"
 send -- "a\r"
+
 sleep 0.1
+
 expect "Multiple devices found... Which device would you like to use?"
 send -- "0\r"
 
