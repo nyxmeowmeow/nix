3d9e5ebbc73c161be5c1c70ed7016ad30a76cfe6
rainmeower <rraaiin@proton.me>
2025-04-17 04:42:15 +1000

    packages.nix

---
+++ packages.nix
@@ -0,0 +1,94 @@
+{ config, pkgs, ... }: {
+
+  environment.systemPackages = with pkgs; [
+    gimp
+    google-chrome
+    # zenity
+    # xdg-desktop-portal-termfilechooser
+    expect
+    nushell
+    zen-browser.packages."x86_64-linux".default 
+    # lmms
+    # godot
+    rustc
+    cargo
+    rust-analyzer
+    clippy
+    unzip
+    termdown
+    tty-clock
+    qview
+    mpc
+    mpd
+    ripgrep
+    nix-inspect
+    mpv
+    obs-studio
+    python3
+    fuse
+    # nss
+    # fuse3
+    # openssl
+    # patchelf
+    # appimage-run
+    libevdev
+    libqalculate
+    hyprprop
+    krita
+    gh
+    netcat
+    jq
+    wl-clipboard
+    clipse
+    eza
+    fzf
+    zoxide
+    waybar
+    walker
+    udiskie
+    udisks2
+    gvfs
+    nautilus
+    gcc
+    wget
+    git
+    dbus
+    firefox
+    vesktop
+    foot
+    hyprland
+    hyprpaper
+    hyprpicker
+    polkit_gnome
+    fish
+    fishPlugins.tide
+    walker
+    grimblast
+    slurp
+    catppuccin-kvantum
+    catppuccin-gtk
+    dunst
+    libnotify
+    playerctl
+    yazi
+    neovim
+    stylua
+    nixd
+    fastfetch
+    kanata
+    dconf
+    mesa
+    libGL
+    libdrm
+    xwayland
+    wine
+    protonup
+    lutris
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.mononoki
+    nerd-fonts.iosevka
+    sn-pro
+  ];
+}
