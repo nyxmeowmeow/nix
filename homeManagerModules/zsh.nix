dcf061df024852c27f77e0eb9e4826386b9df7a2
rainmeower <rraaiin@proton.me>
2025-04-23 16:55:01 +1000

    23/04 16:55:01

--- homeManagerModules/zsh.nix
+++ homeManagerModules/zsh.nix
@@ -1,5 +1,5 @@
-
 { config, pkgs, ... }: {
+
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
@@ -19,28 +19,65 @@
 
     history.path = "$ZDOTDIR/.zsh_history";
 
-    # plugins = [
-    #   {
-    #     # will source zsh-autosuggestions.plugin.zsh
-    #     name = "zsh-autosuggestions";
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "zsh-users";
-    #       repo = "zsh-autosuggestions";
-    #       rev = "v0.4.0";
-    #       sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-    #     };
-    #   }
-    # ];
-
-    prezto = {
+    syntaxHighlighting = {
       enable = true;
-      caseSensitive = false;
-      editor = {
 
-        dotExpansion = true;
-        promptContext = true;
-      };
+    };
 
+    plugins = [
+      {
+        # will source zsh-autosuggestions.plugin.zsh
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.4.0";
+          sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+        };
+      }
+    ];
+
+    # shellGlobalAliases
+
+    shellAliases = {
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      "....." = "cd ../../../..";
+
+      sudo="sudo -k"; # prompt every time
+      s="sudo -k"; # prompt every time
+      rm="rm -i"; # prompt every time
+      mv="mv -i"; # prompt every time
+      ln="ln -i"; # prompt every time
+
+
+      logout="hyprctl dispatch exit 0";
+      c="clear"; # clear terminal
+      l="eza -lh  --icons=auto"; # long list
+      ls="eza -a1   --icons=auto"; # short list
+      ll="eza -lha --icons=auto --sort=name --group-directories-first"; # long list all
+      ld="eza -lhD --icons=auto"; # long list dirs
+      lt="eza --icons=auto --tree"; # list folder as tree
+      ff="fastfetch --logo nixos_small --logo-color-2 magenta";
+      cl="hyprctl clients";
+
+
+
+      ga="git add .";
+      gl="git log --oneline | fzf";
+      gd="git diff HEAD^";
+      gs="git status";
+      cr="cargo run";
+
+
+
+      nr="sudo nixos-rebuild switch --flake /home/meow/nix#nixos && cd ~/nix && git add . && git commit -m $(date +'%d/%m %H:%M:%S')";
+      nro="sudo nixos-rebuild switch --offline --flake /home/meow/nix#nixos && cd ~/nix && git add . && git commit -m $(date +'%d/%m %H:%M:%S')";
+      na="nvim ~/nix/packages.nix -c '/systemPackages'";
+      ns="nix-shell -p";
+
+      n="nvim";
     };
 
   };
