4c598c19056c4a9ba0ffdbc4d54f478e5068708e
rainmeower <rraaiin@proton.me>
2025-04-23 20:05:08 +1000

    23/04 20:05:08

--- homeManagerModules/nushell.nix
+++ homeManagerModules/nushell.nix
@@ -46,7 +46,7 @@
                   modifier: control
                   keycode: char_s
                   mode: emacs
-                  event: Send(EditCommand: ExecuteHostCommand("commandline edit (history | to text | lines | reverse | uniq | fzf)"))
+                  event: Send(EditCommand(ExecuteHostCommand $"commandline edit (history | to text | lines | reverse | uniq | fzf)"))
               }
           ]
       }
