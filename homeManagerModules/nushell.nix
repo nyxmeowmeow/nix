fc0a7f682ce4a0d575378a9c3086cef7804dba1d
rainmeower <rraaiin@proton.me>
2025-04-23 18:26:56 +1000

    23/04 18:26:56

--- homeManagerModules/nushell.nix
+++ homeManagerModules/nushell.nix
@@ -2,16 +2,21 @@
 
   programs.nushell = {
     enable = true;
+
+
+    # configFile.text = ''
+    #   let config = {
+    #   }
+    # '';
+
     envFile.text = ''
-      $env.config = {
-          show_banner = false
-          table.mode = "none"
-          color_config: {
-              shape_garbage: { fg: "#000000" bg: "#FF0000" attr: b}
-              shape_nothing: foreground
-              shape_filepath: green
-              shape_external: blue
-          }
+      $env.config.show_banner = false
+      $env.config.table.mode = "none"
+      $env.config.color_config: {
+          shape_garbage: { fg: "#000000" bg: "#FF0000" attr: b}
+          shape_nothing: foreground
+          shape_filepath: green
+          shape_external: blue
       }
     '';
 
