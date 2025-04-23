97d53446b6f9480bf9f990569b2c1a66ad8c1982
rainmeower <rraaiin@proton.me>
2025-04-23 16:33:03 +1000

    23/04 16:33:03

--- homeManagerModules/zsh.nix
+++ homeManagerModules/zsh.nix
@@ -5,6 +5,19 @@
     enableAutosuggestions = true;
     enableCompletion = true;
     autocd = true;
+    defaultKeymap = "emacs";
+
+    # dirHashes = {
+    #   doc  = "$HOME/Documents";
+    #   vid  = "$HOME/Videos";
+    #   dl   = "$HOME/Downloads";
+    # };
+
+    dotDir = ".config/zsh";
+
+    # history.ignorePatterns = []
+
+    history.path = "$ZDOTDIR/.zsh_history";
 
   };
 }
