5cb4631b88b1cde4b43c952c8570b5294c1a4876
rainmeower <rraaiin@proton.me>
2025-04-25 21:26:10 +1000

    25/04 21:26:10

--- homeManagerModules/nushell.nix
+++ homeManagerModules/nushell.nix
@@ -33,10 +33,11 @@
         }
 
       # convert .mp4 file to .mov
-      def movify [msg: string] {
-        ffmpeg -i $"($msg).mp4" -c:v dnxhd -profile:v dnxhr_hq -c:a pcm_s16le -pix_fmt yuv422p $"($msg).mov"
-      }
-
+def movify [msg: string] {
+  let input = $"($msg).mp4"
+  let output = $"($msg).mov"
+  ^ffmpeg -i $input -c:v dnxhd -profile:v dnxhr_hq -c:a pcm_s16le -pix_fmt yuv422p $output
+}
 
       def nr [...msg: string] {
         sudo nixos-rebuild switch --flake /home/meow/nix#nixos
