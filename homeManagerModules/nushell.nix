f3d476d3208f707bbd1fa11ad3898a5e3ac9f3f9
rainmeower <rraaiin@proton.me>
2025-04-25 21:41:36 +1000

    25/04 21:41:36

--- homeManagerModules/nushell.nix
+++ homeManagerModules/nushell.nix
@@ -37,8 +37,8 @@
       def movify [msg: string] {
         let input = $msg
         let base = (echo $msg | path parse | get stem)
-        let output = "$($base).mov"
-        ^ffmpeg -i $input -c:v dnxhd -profile:v dnxhr_hq -c:a pcm_s16le -pix_fmt yuv422p $output
+        let mov = ".mov"
+        ^ffmpeg -i $input -c:v dnxhd -profile:v dnxhr_hq -c:a pcm_s16le -pix_fmt yuv422p $base$mov
       }
 
 
