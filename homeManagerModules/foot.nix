4d939477020584849670136d33bcce704f0b870d
rainmeower <rraaiin@proton.me>
2025-04-26 02:10:41 +1000

    26/04 02:10:41

--- homeManagerModules/foot.nix
+++ homeManagerModules/foot.nix
@@ -21,30 +21,30 @@
         color = "24273a cad3f5";
       };
 
-      colors = {
-        alpha = "0";
-        background = "24273a";
-        foreground = "cad3f5";
-        ## Normal/regular colors (color palette 0-7)
-        regular0 = "242424";  # black
-        regular1 = "ed8796";  # red
-        regular2 = "a6da95";  # green
-        regular3 = "eed49f";  # yellow
-        regular4 = "b7bdf8";  # blue
-        regular5 = "c6a0f6";  # magenta
-        regular6 = "8bd5ca";  # cyan
-        regular7 = "e6e6e6";  # white
-
-        ## Bright colors (color palette 8-15)
-        bright0 = "616161";   # bright black
-        bright1 = "ed8796";   # bright red
-        bright2 = "a6da95";   # bright green
-        bright3 = "eed49f";   # bright yellow
-        bright4 = "b7bdf8";   # bright blue
-        bright5 = "c6a0f6";   # bright magenta
-        bright6 = "8bd5ca";   # bright cyan
-        bright7 = "ffffff";   # bright white
-      };
+      # colors = {
+      #   alpha = "0";
+      #   background = "24273a";
+      #   foreground = "cad3f5";
+      #   ## Normal/regular colors (color palette 0-7)
+      #   regular0 = "242424";  # black
+      #   regular1 = "ed8796";  # red
+      #   regular2 = "a6da95";  # green
+      #   regular3 = "eed49f";  # yellow
+      #   regular4 = "b7bdf8";  # blue
+      #   regular5 = "c6a0f6";  # magenta
+      #   regular6 = "8bd5ca";  # cyan
+      #   regular7 = "e6e6e6";  # white
+      #
+      #   ## Bright colors (color palette 8-15)
+      #   bright0 = "616161";   # bright black
+      #   bright1 = "ed8796";   # bright red
+      #   bright2 = "a6da95";   # bright green
+      #   bright3 = "eed49f";   # bright yellow
+      #   bright4 = "b7bdf8";   # bright blue
+      #   bright5 = "c6a0f6";   # bright magenta
+      #   bright6 = "8bd5ca";   # bright cyan
+      #   bright7 = "ffffff";   # bright white
+      # };
       key-bindings = {
         scrollback-up-half-page = "Control+Shift+e";
         scrollback-down-half-page = "Control+Shift+a";
