f255c9fc0a222db151b57f6c71de9eadd6c5d519
rainmeower <rraaiin@proton.me>
2025-04-26 02:28:40 +1000

    26/04 02:28:40

--- homeManagerModules/foot.nix
+++ homeManagerModules/foot.nix
@@ -22,7 +22,7 @@
       };
 
       colors = {
-        alpha = "1";
+        alpha = "0";
         background = "24273a";
         foreground = "cad3f5";
         ## Normal/regular colors (color palette 0-7)
