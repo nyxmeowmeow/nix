ff67d9438d79e6d328454509bf48716ac1a1028e
rainmeower <rraaiin@proton.me>
2025-04-25 21:31:24 +1000

    25/04 21:31:24

--- homeManagerModules/nushell.nix
+++ homeManagerModules/nushell.nix
@@ -36,7 +36,7 @@
       # convert .mp4 file to .mov
       def movify [msg: string] {
         let input = $msg
-        let output = "($msg).mov"
+        let output = `$msg.mov`
         ^ffmpeg -i $input -c:v dnxhd -profile:v dnxhr_hq -c:a pcm_s16le -pix_fmt yuv422p $output
       }
 
