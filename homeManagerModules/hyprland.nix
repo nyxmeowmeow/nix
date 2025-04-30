1adeff24b4f6029d48e4312697e4d10057d5fead
rainmeower <rraaiin@proton.me>
2025-04-30 21:26:57 +1000

    30/04 21:26:57

--- homeManagerModules/hyprland.nix
+++ homeManagerModules/hyprland.nix
@@ -23,9 +23,8 @@
     };
 
 
-    gtk3.extraConfig = {
-    gtk-key-theme-name = "Emacs";
-    };
+    gtk3.extraConfig.gtk-key-theme-name = "Emacs";
+    gtk4.extraConfig.gtk-key-theme-name = "Emacs";
   };
 
 
