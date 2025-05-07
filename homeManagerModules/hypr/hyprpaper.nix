5d2a01a598ac9c6125f8d7b349dc2211960ab202
rainmeower <rraaiin@proton.me>
2025-05-07 10:57:26 +1000

    07/05 10:57:26

--- homeManagerModules/hypr/hyprpaper.nix
+++ homeManagerModules/hypr/hyprpaper.nix
@@ -8,14 +8,12 @@
       splash_offset = 2.0;
 
       preload = [
-        "~/nix/stuff/1_hor.png" 
+        "~/nix/stuff/4_hor.png" 
         "~/nix/stuff/1_vert.png" 
-        "~/nix/stuff/2_hor.jpg" 
-        "~/nix/stuff/3_hor.jpg" 
       ];
 
       wallpaper = [
-        "DP-1,~/nix/stuff/3_hor.jpg"
+        "DP-1,~/nix/stuff/4_hor.jpg"
         "DP-3,~/nix/stuff/1_vert.png"
       ];
     };
