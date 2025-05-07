be036ace592dc05513fc7ece5cc1e64e150a9b27
rainmeower <rraaiin@proton.me>
2025-05-07 10:42:25 +1000

    07/05 10:42:25

--- homeManagerModules/hypr/hyprpaper.nix
+++ homeManagerModules/hypr/hyprpaper.nix
@@ -10,11 +10,11 @@
       preload = [
         "~/nix/stuff/1_hor.png" 
         "~/nix/stuff/1_vert.png" 
-        "~/nix/stuff/2_hor.png" 
+        "~/nix/stuff/2_hor.jpg" 
       ];
 
       wallpaper = [
-        "DP-1,~/nix/stuff/2_hor.png"
+        "DP-1,~/nix/stuff/2_hor.jpg"
         "DP-3,~/nix/stuff/1_vert.png"
       ];
     };
