bac743f7cc4ea4e58e890c7e85e4543ffbeeeb98
rainmeower <rraaiin@proton.me>
2025-05-19 12:18:54 +1000

    19/05 12:18:54

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -145,7 +145,7 @@
     ((key-history a 1)) (macro o) break
     ((key-history u 1)) (macro e) break
     ((key-history e 1)) (macro u) break
-    ((key-history e 2) (key-history r 1)) (macro e) break
+    ((and(key-history e 2) (key-history r 1))) (macro e) break
     ((key-history w 1)) (macro y) break
     ;;((key-history y 1)) (macro p) break
     ((key-history j 1)) (macro u s t) break
