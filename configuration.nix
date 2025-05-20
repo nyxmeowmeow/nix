48db12ad6c8e63d5cb714eba4f444ffffb6eee76
rainmeower <rraaiin@proton.me>
2025-05-20 23:37:35 +1000

    20/05 23:37:35

--- configuration.nix
+++ configuration.nix
@@ -12,8 +12,11 @@
 
 
   boot = {
-    loader.systemd-boot.enable = true;
-    loader.efi.canTouchEfiVariables = true;
+    loader = {
+    systemd-boot.enable = true;
+    systemd-boot.configurationLimit = 20;
+    efi.canTouchEfiVariables = true;
+    };
     initrd.kernelModules = [ "amdgpu" ];
     kernelPackages = pkgs.linuxPackages_zen;
     kernelParams = [
