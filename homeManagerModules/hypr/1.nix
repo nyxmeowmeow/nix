f32d7be37b93a9d487eb7619ae39776d7a496141
rainmeower <rraaiin@proton.me>
2025-05-26 22:54:31 +1000

    26/05 22:54:31

--- homeManagerModules/hypr/1.nix
+++ homeManagerModules/hypr/1.nix
@@ -5,7 +5,7 @@
       general = {
         gaps_in = 4;
         gaps_out = 8;
-        border_size = 3;
+        border_size = 2;
         "col.active_border" = "rgba(b7bdf8ff)";
         "col.inactive_border" = "rgba(24273a00)";
       };
