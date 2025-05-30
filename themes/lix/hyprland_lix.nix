2a0c4aeffff163c030e120f3f5af2f5e1804430a
rainmeower <rraaiin@proton.me>
2025-05-31 00:42:37 +1000

    31/05 00:42:37

--- themes/lix/hyprland_lix.nix
+++ themes/lix/hyprland_lix.nix
@@ -6,8 +6,8 @@
         gaps_in = 4;
         gaps_out = 8;
         border_size = 2;
-        "col.active_border" = "rgba(b7bdf8ff)";
-        "col.inactive_border" = "rgba(24273a00)";
+        "col.active_border" = "rgba(DBBFEFFF)";
+        "col.inactive_border" = "rgba(00000000)";
       };
 
 
