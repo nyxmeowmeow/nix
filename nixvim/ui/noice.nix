d28c9722306f2ed9a8a6ea6737ac92a67d3624f9
rainmeower <rraaiin@proton.me>
2025-05-31 07:34:51 +1000

    31/05 07:34:51

---
+++ nixvim/ui/noice.nix
@@ -0,0 +1,33 @@
+{
+programs.nixvim.plugins = {
+noice = {
+enable = true;
+settings = {
+cmdline = {
+enabled = true;
+format = {
+      cmdline = { pattern = "^:"; icon = "󰘧"; lang = "vim"; };
+      search_down = { kind = "search"; pattern = "^/"; icon = " "; lang = "regex"; };
+      search_up = { kind = "search"; pattern = "^%?"; icon = " "; lang = "regex"; };
+      filter = { pattern = "^:%s*!"; icon = "$"; lang = "bash"; };
+      lua = { 
+      pattern = [
+      "^:%s*lua%s+"
+      "^:%s*lua%s*=%s*"
+      "^:%s*=%s*"
+      ];
+      icon = "";
+      lang = "lua";
+      };
+
+      help = { pattern = "^:%s*he?l?p?%s+"; icon = ""; };
+};
+};
+};
+
+};
+
+nui.enable = true;
+nvim-notify.enable = true;
+};
+}
