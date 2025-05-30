fd3bc81198ce0116c476a47b0d606d9489891c8c
rainmeower <rraaiin@proton.me>
2025-05-31 01:10:24 +1000

    a

--- home.nix
+++ home.nix
@@ -7,17 +7,17 @@
     # ./themes/macchiato/.macchiato_theme.nix
 
 
-    ./homeManagerModules/hyprland.nix
-    ./homeManagerModules/nushell.nix
-    ./homeManagerModules/starship.nix
-    # ./homeManagerModules/yazi.nix
-    ./homeManagerModules/clipse.nix
-    ./homeManagerModules/ncmpcpp.nix
-    ./homeManagerModules/btop.nix
-    ./homeManagerModules/git.nix
-    ./homeManagerModules/dunst.nix
-    ./homeManagerModules/mpv.nix
-    ./homeManagerModules/vesktop.nix
+    ./home_modules/hyprland.nix
+    ./home_modules/nushell.nix
+    ./home_modules/starship.nix
+    # ./home_modules/yazi.nix
+    ./home_modules/clipse.nix
+    ./home_modules/ncmpcpp.nix
+    ./home_modules/btop.nix
+    ./home_modules/git.nix
+    ./home_modules/dunst.nix
+    ./home_modules/mpv.nix
+    ./home_modules/vesktop.nix
   ];
 
   home.username = "meow";
