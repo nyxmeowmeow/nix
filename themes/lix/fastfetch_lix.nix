82d8b55895f63e80fc103e5aa3e730f34d0d9e41
rainmeower <rraaiin@proton.me>
2025-05-30 23:34:35 +1000

    30/05 23:34:35

--- themes/lix/fastfetch_lix.nix
+++ themes/lix/fastfetch_lix.nix
@@ -20,17 +20,17 @@
         {
           type = "os";
           key = "os";
-          keyColor = "#A4A0E8";
+          keyColor = "magenta";
         }
         {
           type = "kernel";
           key = "kn";
-          keyColor = "#A4A0E8";
+          keyColor = "magenta";
         }
         {
           type = "packages";
           key = "pk";
-          keyColor = "#A4A0E8";
+          keyColor = "magenta";
         }
         # {
         #   type = "display";
@@ -40,34 +40,34 @@
         # {
         #   type = "wm";
         #   key = "wm";
-        #   keyColor = "#A4A0E8";
+        #   keyColor = "magenta";
         # }
         {
           type = "command";
           key = "wm";
-          keyColor = "#A4A0E8";
+          keyColor = "magenta";
           text = "echo hyprland";
         }
         # {
         #   type = "terminal";
         #   key = "tm";
-        #   keyColor = "#A4A0E8";
+        #   keyColor = "magenta";
         # }
         {
           type = "command";
           key = "tm";
-          keyColor = "#A4A0E8";
+          keyColor = "magenta";
           text = "echo foot";
         }
         # {
         #   type = "shell";
         #   key = "sh";
-        #   keyColor = "#A4A0E8";
+        #   keyColor = "magenta";
         # }
         {
           type = "command";
           key = "sh";
-          keyColor = "#A4A0E8";
+          keyColor = "magenta";
           text = "echo nushell";
         }
         "break"
