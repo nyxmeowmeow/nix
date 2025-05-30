da5c3ce1707bffece22e04b177b234f8915e0baf
rainmeower <rraaiin@proton.me>
2025-05-31 04:45:03 +1000

    31/05 04:45:03

--- nixvim/lualine.nix
+++ nixvim/lualine.nix
@@ -1,21 +1,53 @@
 { ... }: {
-		programs.nixvim.plugins.lualine = {
-			enable = true;
-			settings = {
-    icons_enabled = true;
+	programs.nixvim.plugins.lualine = {
+		enable = true;
+		settings = {
+			icons_enabled = true;
+		always_show_tabline = true;
 
-    theme = {
-      normal = { c = { 
-      fg = "#A4A0E8";
-      bg = "#00000000";
-      }; 
-      };
-      inactive = { c = {
-      fg = "#A4A0E8";
-      bg = "#00000000";
-      };
-      };
-    };
+			theme = {
+				normal = { c = { 
+					fg = "#A4A0E8";
+					bg = "#00000000";
+				}; 
+				};
+				inactive = { c = {
+					fg = "#A4A0E8";
+					bg = "#00000000";
+				};
+				};
+			};
+
+			component_separators = {
+				left = "";
+				right = "";
+			};
+			section_separators = {
+				left = "";
+				right = "";
+			};
+
+			sections = {
+				lualine_a = [
+					"buffers"
+					{
+						buffers_color = {
+							active = {
+								bg = "#A4A0E8";
+								fg = "#3B224C";
+							};
+							inactive = {
+								bg = "#00000000";
+								fg = "#A4A0E8";
+							};
+						};
+						symbols = {
+							modified = " ";
+							alternate_file = "";
+						};
+					}
+				];
 			};
 		};
-		}
+	};
+	 }
