ba444f4429371fb6bfbd119392c58f053738a611
rainmeower <rraaiin@proton.me>
2025-05-31 04:14:00 +1000

    31/05 04:14:00

--- nixvim/colorscheme.nix
+++ nixvim/colorscheme.nix
@@ -9,7 +9,26 @@
 				gitsigns = true;
 				noice = true;
 
+			};
+			no_italic = false;
+			no_bold = false;
+			no_underline = false;
+
+
+			color_overrides.all = {
+				text = "#A4A0E8";
+				base = "#3B224C";
+
+#lilac = "#C7B8E0";
+#green = "#A5E07F"; #a6da95
+#white = "#d5e7ff";
+#orange = "#EFBA5D";
+#bubblegum = "#D678B5";
+#magenta = "#C78DFC";
+#red = "#F22C86";
+#violet = "#6F44F0";
+#pink = "#DBBFEF";
+			};
 		};
 	};
-    };
-}
+	 }
---
+++ nixvim/highlight-colors.nix
@@ -0,0 +1,7 @@
+{ ... }: {
+programs.nixvim.plugins.nvim-highlight-colors = {
+enable = true;
+virtual_symbol = "󰧞";
+enable_named_colors = true;
+};
+}
