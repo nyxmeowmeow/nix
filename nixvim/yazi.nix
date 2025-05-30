794f322fada4327a80e6505222ab8b3eb59e2ec8
rainmeower <rraaiin@proton.me>
2025-05-31 06:21:29 +1000

    31/05 06:21:29

--- nixvim/yazi.nix
+++ nixvim/yazi.nix
@@ -1,7 +1,22 @@
-{ ... }: {
-programs.nixvim.plugins.yazi = {
+{
+programs.nixvim = {
+plugins.yazi = {
 enable = true;
-
-
+#yazi_floating_window_border = "none";
+#highlight_hovered_buffers_in_same_directory = false;
 };
+
+
+
+
+
+	keymaps = [
+    {
+      key = "<leader>f";
+      mode = [ "n" "v" ];
+      action = "<cmd>Yazi<cr>";
+    }
+    ];
+    };
+
 }
