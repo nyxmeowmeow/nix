c9aeabec8f594a46b703db6919a938c5a1f39598
rainmeower <rraaiin@proton.me>
2025-05-30 22:43:37 +1000

    30/05 22:43:37

--- themes/lix/hyprpaper_lix.nix
+++ themes/lix/hyprpaper_lix.nix
@@ -8,13 +8,12 @@
       splash_offset = 2.0;
 
       preload = [
-        "~/nix/themes/lix/wallpapers/1_hor.png" 
-        "~/nix/themes/lix/wallpapers/1_vert.png" 
+        "~/nix/themes/lix/wallpapers/2_hor.png" 
       ];
 
       wallpaper = [
-        "DP-1, ~/nix/themes/lix/wallpapers/1_hor.png"
-        "DP-3, ~/nix/themes/lix/wallpapers/1_vert.png"
+        "DP-1, ~/nix/themes/lix/wallpapers/2_hor.png"
+        "DP-3, ~/nix/themes/lix/wallpapers/2_hor.png"
       ];
     };
   };
