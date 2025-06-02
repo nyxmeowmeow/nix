764ec80ffb65ca37aac3401b04ffb8cf75ee7cdb
rainmeower <rraaiin@proton.me>
2025-06-02 14:32:24 +1000

    02/06 14:32:24

--- nixvim/utils/unimpaired.nix
+++ nixvim/utils/unimpaired.nix
@@ -2,7 +2,22 @@
   programs.nixvim = {
     extraPlugins = [ pkgs.vimPlugins.mini-bracketed ];
     extraConfigLua = ''
-      require('mini.bracketed').setup()
+      require('mini.bracketed').setup({
+  buffer     = { suffix = 'r', options = {} },
+  comment    = { suffix = 'w', options = {} },
+  conflict   = { suffix = 'x', options = {} },
+  diagnostic = { suffix = 's', options = {} },
+  file       = { suffix = 'm', options = {} },
+  indent     = { suffix = 'l', options = {} },
+  jump       = { suffix = 'y', options = {} },
+  location   = { suffix = 'p', options = {} },
+  oldfile    = { suffix = 'd', options = {} },
+  quickfix   = { suffix = 'q', options = {} },
+  treesitter = { suffix = 'b', options = {} },
+  undo       = { suffix = 'u', options = {} },
+  window     = { suffix = 'v', options = {} },
+  yank       = { suffix = 'f', options = {} },
+      })
     '';
   };
 }
