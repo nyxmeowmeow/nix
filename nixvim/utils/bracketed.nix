a5412d2ed5521be7d52e813e35288f616a96859c
rainmeower <rraaiin@proton.me>
2025-06-02 15:03:58 +1000

    02/06 15:03:58

--- nixvim/utils/bracketed.nix
+++ nixvim/utils/bracketed.nix
@@ -4,7 +4,20 @@
   settings = {
   keymaps = [
   { buffer = "r"; }
+  { comment = "w"; }
+  { conflict = "x"; }
+  { diagnostic = "s"; }
+  { file = "m"; }
+  { indent = "l"; }
+  { jump = "y"; }
+  { location = "p"; }
+  { oldfile = "d"; }
+  { quickfix = "q"; }
+  { treesitter = "b"; }
+  { undo = "u"; }
+  { window = "v"; }
+  { yank = "f"; }
   ];
   };
-};
+  };
 }
