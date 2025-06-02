a17e40fc526646ae49c31967ce888db1c0ac8cd1
rainmeower <rraaiin@proton.me>
2025-06-02 10:13:24 +1000

    02/06 10:13:24

--- nixvim/utils/indentscope.nix
+++ nixvim/utils/indentscope.nix
@@ -2,15 +2,19 @@
   programs.nixvim = {
     extraPlugins = [ pkgs.vimPlugins.mini-indentscope ];
     extraConfigLua = ''
-      require('mini.indentscope').setup()
+      require('mini.indentscope').setup({
+          draw = {
+              delay = 100,
+          },
+          symbol = "│";
+      })
     '';
-    plugins.mini-indentscope = {
-      enable = true;
-      settings = {
-        draw.delay = 100;
-        symbol = "│";
-      };
-
-    };
+#    plugins.mini-indentscope = {
+#      enable = true;
+#      settings = {
+#        draw.delay = 100;
+#        symbol = "│";
+#      };
+#    };
   };
 }
