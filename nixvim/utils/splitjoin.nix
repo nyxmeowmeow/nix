de15a90a05261fcfc7e41b9fd47c39182e6499c8
rainmeower <rraaiin@proton.me>
2025-06-02 15:42:25 +1000

    02/06 15:42:25

--- nixvim/utils/splitjoin.nix
+++ nixvim/utils/splitjoin.nix
@@ -1,13 +1,10 @@
 {
   programs.nixvim = {
-    plugins.mini.modules.splitjoin.pairs.enable = true;
-    keymaps = [
-    {
-      key = "ga";
-      action = "MiniSplitjoin.toggle()";
-      mode = [ "n" "v" "x" ];
-      lua = true;
-    }
-    ];
+    plugins.mini.modules.splitjoin.pairs = {
+      enable = true;
+      keymaps = {
+        "ga" = "toggle";
+      };
+    };
   };
 }
