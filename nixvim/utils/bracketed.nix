b2ee85768dd7f235d53a844650be8c72a5a7bbd6
rainmeower <rraaiin@proton.me>
2025-06-02 14:59:55 +1000

    02/06 14:59:55

--- nixvim/utils/bracketed.nix
+++ nixvim/utils/bracketed.nix
@@ -1,24 +1,10 @@
 { pkgs, ... }: {
-  programs.nixvim = {
-  plugins.mini.enable = true;
-    extraPlugins = [ pkgs.vimPlugins.mini-bracketed ];
-    extraConfigLua = ''
-      require('mini.bracketed').setup({
-  buffer     = { suffix = 'r', options = {} },
-  comment    = { suffix = 'w', options = {} },
-  conflict   = { suffix = 'x', options = {} },
-  diagnostic = { suffix = 's', options = {} },
-  file       = { suffix = 'm', options = {} },
-  indent     = { suffix = 'l', options = {} },
-  jump       = { suffix = 'y', options = {} },
-  location   = { suffix = 'p', options = {} },
-  oldfile    = { suffix = 'd', options = {} },
-  quickfix   = { suffix = 'q', options = {} },
-  treesitter = { suffix = 'b', options = {} },
-  undo       = { suffix = 'u', options = {} },
-  window     = { suffix = 'v', options = {} },
-  yank       = { suffix = 'f', options = {} },
-      })
-    '';
+  programs.nixvim.plugins.mini.modules.bracketed = {
+  enable = true;
+  settings = {
+  keymaps = [
+  { buffer = "r"; }
+  ];
   };
+};
 }
