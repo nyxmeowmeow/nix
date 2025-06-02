e4a6e72c99b6ed151cdd8dd2c70243e2b41d1f79
rainmeower <rraaiin@proton.me>
2025-06-02 15:06:27 +1000

    02/06 15:06:27

--- nixvim/utils/bracketed.nix
+++ nixvim/utils/bracketed.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{
   programs.nixvim.plugins.mini.modules.bracketed = {
   enable = true;
   settings = {
--- nixvim/utils/indentscope.nix
+++ nixvim/utils/indentscope.nix
@@ -1,20 +1,9 @@
-{ pkgs, ... }: {
-  programs.nixvim = {
-    extraPlugins = [ pkgs.vimPlugins.mini-indentscope ];
-    extraConfigLua = ''
-      require('mini.indentscope').setup({
-          draw = {
-              delay = 100,
-          },
-          symbol = "│"; -- ⎸
-      })
-    '';
-#    plugins.mini-indentscope = {
-#      enable = true;
-#      settings = {
-#        draw.delay = 100;
-#        symbol = "│";
-#      };
-#    };
+{
+  programs.nixvim.plugins.mini.modules.indentscope = {
+    enable = true;
+    settings = {
+      draw.delay = 100;
+      symbol = "│";
+    };
   };
 }
