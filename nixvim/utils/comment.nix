a40019d7a22291727f251fdbda8ecd57b4e25306
rainmeower <rraaiin@proton.me>
2025-06-03 21:28:36 +1000

    03/06 21:28:36

--- nixvim/utils/comment.nix
+++ nixvim/utils/comment.nix
@@ -3,16 +3,16 @@
     plugins.mini.modules.comment = {
       enable = true;
 
-    lazyLoad.settings.event = [
-      "DeferredUIEnter"
-    ];
+      lazyLoad.settings.event = [
+        "DeferredUIEnter"
+      ];
 
       settings = {
         ignore_blank_line = true;
       };
       keymaps = {
         comment = "x";
-        comment_line = "X";
+        comment_line = "gx";
         comment_visual = "x";
         textobject = "x";
       };
