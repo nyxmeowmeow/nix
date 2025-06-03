660d57fe65201982a4ceba676c504afa0f2eec64
rainmeower <rraaiin@proton.me>
2025-06-03 22:55:23 +1000

    03/06 22:55:23

--- nixvim/utils/harpoon.nix
+++ nixvim/utils/harpoon.nix
@@ -1,12 +1,13 @@
 {
-  plugins.harpoon.enable = true;
+  programs.nixvim = {
+    plugins.harpoon.enable = true;
 
-  keymaps = [
+    keymaps = [
     {
       mode = "n";
       key = "<leader>jh";
       action = ":lua require('harpoon.ui').toggle_quick_menu()<CR>";
-      #    lua = true;
+#    lua = true;
       options = {
         silent = true;
         desc = "Mark Menu";
@@ -17,7 +18,7 @@
       mode = "n";
       key = "<leader>jj";
       action = ":lua require('harpoon.mark').add_file()<CR>";
-      #    lua = true;
+#    lua = true;
       options = {
         silent = true;
         desc = "Mark File";
@@ -27,7 +28,7 @@
       mode = "n";
       key = "<leader>jl";
       action = ":lua require('harpoon.ui').nav_next()<CR>";
-      #    lua = true;
+#    lua = true;
       options = {
         silent = true;
         desc = "Next Mark";
@@ -38,11 +39,12 @@
       mode = "n";
       key = "<leader>jh";
       action = ":lua require('harpoon.ui').nav_prev()<CR>";
-      #    lua = true;
+#    lua = true;
       options = {
         silent = true;
         desc = "Prev Mark";
       };
     }
-  ];
+    ];
+  };
 }
