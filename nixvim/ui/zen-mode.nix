6b0f78633ca37e46023af7a912b2ed5ab3feb762
rainmeower <rraaiin@proton.me>
2025-06-04 00:05:17 +1000

    04/06 00:05:17

--- nixvim/ui/zen-mode.nix
+++ nixvim/ui/zen-mode.nix
@@ -2,6 +2,17 @@
   programs.nixvim.plugins.zen-mode = {
     enable = true;
 
+    settings = {
+      signcolumn = false;
+      number = false;
+      relativenumber = false;
+      cursorline = false;
+      cursorcolumn = false;
+      foldcolumn = false;
+
+
+    };
+
     lazyLoad.settings.event = [
       "DeferredUIEnter"
     ];
