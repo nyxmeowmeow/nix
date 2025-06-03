2077e8d8c206ecd8e0a4037767603e2b55f86561
rainmeower <rraaiin@proton.me>
2025-06-03 16:42:31 +1000

    03/06 16:42:31

--- nixvim/utils/treesitter-textobjects.nix
+++ nixvim/utils/treesitter-textobjects.nix
@@ -1,5 +1,16 @@
 {
   programs.nixvim.plugins.treesitter-textobjects = {
     enable = true;
+    select = {
+      enable = true;
+      lookahead = true;
+      keymaps = {
+        "af" = "@function.outer";
+        "if" = "@function.inner";
+        "ac" = "@class.outer";
+        "ic" = "@class.inner";
+        "as" = "@local.scope";
+      };
+    };
   };
 }
