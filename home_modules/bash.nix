58329735734a8de7f899cebed14ca3729ad97315
rainmeower <rraaiin@proton.me>
2025-06-04 14:05:07 +1000

    04/06 14:05:07

--- home_modules/bash.nix
+++ home_modules/bash.nix
@@ -5,18 +5,18 @@
     historyFile = "~/.config";
     historyIgnore = [
       "ls"
-        "nr"
-        "ng"
-        "mkdir"
+      "nr"
+      "ng"
+      "mkdir"
     ];
     sessionVariables = {
       MANPAGER = "nvim +Man!"; # use nvim for man
-        SUDO_TIMESTAMP_TIMEOUT = 0;
+      SUDO_TIMESTAMP_TIMEOUT = 0;
       EDITOR = "nvim";
       VISUAL = "nvim";
       BROWSER = "zen";
       NIXOS_OZONE_WL = "1"; # tell things to use wayland
-        NIXPKGS_ALLOW_UNFREE = "1";
+      NIXPKGS_ALLOW_UNFREE = "1";
     };
 
     shellAliases = {
@@ -28,10 +28,10 @@
       hist="history | fzf | read -l command; eval $command";
 
       sudo = "sudo -k"; # prompt every time
-        s = "sudo -k"; # prompt every time
-        rm = "rm -i"; # prompt every time
-        mv = "mv -i"; # prompt every time
-        ln = "ln -i"; # prompt every time
+      s = "sudo -k"; # prompt every time
+      rm = "rm -i"; # prompt every time
+      mv = "mv -i"; # prompt every time
+      ln = "ln -i"; # prompt every time
 
         logout = "hyprctl dispatch exit 0";
 # l = "eza -lh  --icons=auto"; # long list
@@ -39,7 +39,7 @@
 # ll = "eza -lha --icons=auto --sort=name --group-directories-first"; # long list all
 # ld = "eza -lhD --icons=auto"; # long list dirs
 # lt = "eza --icons=auto --tree"; # list folder as tree
-        ff = "fastfetch --logo nixos_small --logo-color-2 magenta";
+      ff = "fastfetch --logo nixos_small --logo-color-2 magenta";
 
 
 
@@ -52,13 +52,13 @@
 
       meow = "echo :3"; # so silly
 
-        na = "nvim ~/nix/packages.nix -c '/systemPackages'";
+      na = "nvim ~/nix/packages.nix -c '/systemPackages'";
       ns = "nix-shell -p";
 
       n = "nvim";
     };
     initExtra = ''
-      source ${pkgs.bashInteractive}/etc/profile.d/bash_completion.sh
+    source ${pkgs.bash-completion}
     '';
   };
 }
