bf7e430cd1a8a33527fb53755900c03fcc01537a
rainmeower <rraaiin@proton.me>
2025-06-04 13:27:08 +1000

    04/06 13:27:08

--- home_modules/fzf.nix
+++ home_modules/fzf.nix
@@ -16,7 +16,6 @@ programs.fzf = {
     border = "#3B224C";
   };
 
-      FZF_DEFAULT_OPTS = "--color=fg:#A4A0E8,hl:#DBBFEF,fg+:#A4A0E8,bg+:#5A3D6E,hl+:#DBBFEF,info:#886C9C,prompt:#DBBFEF,spinner:-1,pointer:-1,gutter:#3B224C,info:#886C9C,border:#3B224C";
 };
 
 }
