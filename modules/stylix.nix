57e05de2af79db3578806ee70e60a604c3a2d1e0
rainmeower <rraaiin@proton.me>
2025-06-07 09:59:39 +1000

    07/06 09:59:39

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -8,25 +8,6 @@
   #   qt.enable = true;
   # };
 
-  stylix.base16Scheme = {
-    pink = "#f5bde6";
-    mauve = "#c6a0f6";
-    red = "#ed8796";
-    peach = "#f5a97f";
-    yellow = "#eed49f";
-    green = "#a6da95";
-    blue = "#8aadf4";
-    lavender = "#b7bdf8";
-    text = "#cad3f5";
-    subtext = "#a5adcb";
-    overlay = "#6e738d";
-    surface1 = "#494d64";
-    surface = "#363a4f";
-    base = "#24273a";
-    mantle = "#1e2030";
-    crust = "#181926";
-  };
-
 
   stylix.image = ../themes/macchiato/wallpapers/1_hor.png; # must be set for some reason
 
---
+++ themes/black/colors_black.nix
@@ -0,0 +1,18 @@
+{
+    base00 = "#000000"; # base
+    base01 = "#181926"; # crust
+    base02 = "#1e2030"; # mantle
+    base03 = "#363a4f"; # surface0
+    base04 = "#494d64"; # surface1
+    base05 = "#6e738d"; # overlay
+    base07 = "#a5adcb"; # subtext
+    base06 = "#cad3f5"; # text
+    base08 = "#b7bdf8"; # lavender
+    base09 = "#8aadf4"; # blue
+    base0A = "#c6a0f6"; # mauve
+    base0B = "#f5bde6"; # pink
+    base0C = "#ed8796"; # red
+    base0D = "#f5a97f"; # peach
+    base0E = "#eed49f"; # yellow
+    base0F = "#a6da95"; # green
+}
