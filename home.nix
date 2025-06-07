3ffcf70c1c0998f8c83de0517e507face18bf392
rainmeower <rraaiin@proton.me>
2025-06-07 13:24:04 +1000

    07/06 13:24:04

--- home.nix
+++ home.nix
@@ -1,9 +1,9 @@
 {
 
   imports = [
-    ./themes/black/default.nix
+    # ./themes/black/default.nix
     # ./themes/lix/default.nix
-    # ./themes/macchiato/default.nix
+    ./themes/macchiato/default.nix
 
     ./home_modules/hyprland.nix
     ./home_modules/nushell.nix
