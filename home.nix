79943c10f4376480c4183702c7a6f53dd04216f6
rainmeower <rraaiin@proton.me>
2025-06-10 20:31:09 +1000

    10/06 20:31:09

--- home.nix
+++ home.nix
@@ -1,42 +1,52 @@
-{ mrrp, ... }: {
+{ config, ... }: {
+  home-manager = {
+    extraSpecialArgs = {
+      systemTheme = config.mrrp.theme;
+    };
+    users.meow = {
 
-  theme = mrrp;
-
-  imports = [
-    # ./themes/black/default.nix
-    ./themes/lix/default.nix
-    # ./themes/macchiato/default.nix
-    ./themes/theme.nix
-
-    ./home_modules/hyprland.nix
-    ./home_modules/nushell.nix
-    ./home_modules/starship.nix
-    # ./home_modules/yazi.nix
-    ./home_modules/clipse.nix
-    ./home_modules/ncmpcpp.nix
-    ./home_modules/btop.nix
-    ./home_modules/git.nix
-    # ./home_modules/bash.nix
-    ./home_modules/fzf.nix
-    ./home_modules/zoxide.nix
-    ./home_modules/dunst.nix
-    ./home_modules/mpv.nix
-    ./home_modules/vesktop.nix
-  ];
-
-  home.username = "meow";
-  home.homeDirectory = "/home/meow";
-  home.stateVersion = "24.11";
-
-  # home.sessionVariables
+  mrrp.theme = "macchiato";
 
 
-  # xdg.desktopEntries = {
-  #   fish.noDisplay = true;
-  #   yazi.noDisplay = true;
-  #   btop.noDisplay = true;
-  # };
+# import = [ ./home.nix ];
+
+
+      imports = [
+# ./themes/black/default.nix
+        ./themes/lix/default.nix
+# ./themes/macchiato/default.nix
+          ./themes/theme.nix
+
+          ./home_modules/hyprland.nix
+          ./home_modules/nushell.nix
+          ./home_modules/starship.nix
+# ./home_modules/yazi.nix
+          ./home_modules/clipse.nix
+          ./home_modules/ncmpcpp.nix
+          ./home_modules/btop.nix
+          ./home_modules/git.nix
+# ./home_modules/bash.nix
+          ./home_modules/fzf.nix
+          ./home_modules/zoxide.nix
+          ./home_modules/dunst.nix
+          ./home_modules/mpv.nix
+          ./home_modules/vesktop.nix
+      ];
+
+      home.username = "meow";
+      home.homeDirectory = "/home/meow";
+      home.stateVersion = "24.11";
 
 
 
-}
+
+
+
+
+
+
+    };
+
+
+  };
+                 }
