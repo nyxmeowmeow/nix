c07cb4fdb4e48ab5f96cb0d6483580311de2696e
rainmeower <rraaiin@proton.me>
2025-06-10 18:41:28 +1000

    10/06 18:41:28

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -128,13 +128,7 @@
   ;;sym (tap-hold-press 120 120 enter (layer-toggle syms))
   wi (tap-hold-press 120 120 enter (layer-toggle win))
   lc (tap-hold-press 120 120 - lctl)
-lm (multi 
-nop1
-(tap-hold-press 120 120 esc lmet)
-)
-
-
-
+  lm (tap-hold-press 120 120 esc lmet)
   rs (tap-hold-press 120 120 bspc lsft)
   ;;col (tap-hold-press 120 120 S-; lctl)
 
@@ -144,13 +138,10 @@ nop1
   ;;dst (dynamic-macro-record-stop-truncate 1)
 
 
-win (switch
-    (nop1) (layer-while-held win) break
+  win (switch
     (lmet rmet) (layer-while-held win) break
     () g break
   )
-
-
 
   cw (caps-word-custom 1000
     (q b f d w p l o u j x n s t c y m h a e i k g v)
