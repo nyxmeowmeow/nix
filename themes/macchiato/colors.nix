a71ccc543cb500300bca01431ed23c4f6855c23d
rainmeower <rraaiin@proton.me>
2025-06-11 21:52:43 +1000

    11/06 21:52:43

--- themes/macchiato/colors.nix
+++ themes/macchiato/colors.nix
@@ -1,21 +1,18 @@
 {
-  theme = {
-    base00 = "#24273a";
-    base01 = "#cad3f5"; # text
-    base02 = "#181926"; # crust
-    base03 = "#1e2030"; # mantle
-    base04 = "#363a4f"; # surface0
-    base05 = "#494d64"; # surface1
-    base06 = "#6e738d"; # overlay
-    base07 = "#a5adcb"; # subtext
-    base08 = "#b7bdf8"; # lavender
-    base09 = "#8aadf4"; # blue
-    base0A = "#c6a0f6"; # mauve
-    base0B = "#f5bde6"; # pink
-    base0C = "#ed8796"; # red
-    base0D = "#f5a97f"; # peach
-    base0E = "#eed49f"; # yellow
-    base0F = "#a6da95"; # green
-  };
+  base00 = "#24273a";
+  base01 = "#cad3f5"; # text
+  base02 = "#181926"; # crust
+  base03 = "#1e2030"; # mantle
+  base04 = "#363a4f"; # surface0
+  base05 = "#494d64"; # surface1
+  base06 = "#6e738d"; # overlay
+  base07 = "#a5adcb"; # subtext
+  base08 = "#b7bdf8"; # lavender
+  base09 = "#8aadf4"; # blue
+  base0A = "#c6a0f6"; # mauve
+  base0B = "#f5bde6"; # pink
+  base0C = "#ed8796"; # red
+  base0D = "#f5a97f"; # peach
+  base0E = "#eed49f"; # yellow
+  base0F = "#a6da95"; # green
 }
-
