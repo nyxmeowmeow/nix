e7925bc45913f47ac9e17e7d6ceebfb2be10fbb7
rainmeower <rraaiin@proton.me>
2025-06-11 17:31:17 +1000

    11/06 17:31:17

---
+++ themes/everforest/hard/colors.nix
@@ -0,0 +1,23 @@
+{
+  bg_dim = "#1E2326";
+  bg     = "#272E33";
+  bg1    = "#2E383C";
+  bg2    = "#374145";
+  bg3    = "#414B50";
+  bg4    = "#495156";
+  bg5    = "#4F5B58";
+  bg_red    = "#4C3743";
+  bg_visual = "#493B40";
+  bg_yellow = "#45443C";
+  bg_green  = "#3C4841";
+  bg_blue   = "#384B55";
+
+  fg     = "#D3C6AA";
+  red    = "#E67E80";
+  orange = "#E69875";
+  yellow = "#DBBC7F";
+  green  = "#A7C080";
+  blue   = "#7FBBB3";
+  aqua   = "#83C092";
+  purple = "#D699B6";
+}
