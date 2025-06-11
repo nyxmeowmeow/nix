486431e37dd9558e8895ab09fd8c6eb4d9cfe7f1
rainmeower <rraaiin@proton.me>
2025-06-11 23:40:21 +1000

    11/06 23:40:21

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -165,6 +165,18 @@ r @cw
 
 (deflayermap (typing) 
  a (unshift a) b (unshift b) c (unshift c) d (unshift d) e (unshift e) f (unshift f) g (unshift g) h (unshift h) i (unshift i) j (unshift j) k (unshift k) l (unshift l) m (unshift m) n (unshift n) o (unshift o) p (unshift p) q (unshift q) r (unshift r) s (unshift s) t (unshift t) u (unshift u) v (unshift v) w (unshift w) x (unshift x) y (unshift y) z (unshift z) 
+
+f15 S--
+f16 S-9
+f17 S-0
+lmet (multi nop1 (tap-hold-press 120 120 esc lmet))
+lsft @magic
+rsft bspc
+ralt tab
+f18 S-;
+lctrl -
+bspc @rpeat
+
  ;;caps XX ;; 5. key deactivations of caps, numbers, ... are optional
 )
 
