eb8b37bcc4f60f46afb8c6e8e02cbbf78a72cf0a
rainmeower <rraaiin@proton.me>
2025-06-11 18:12:29 +1000

    11/06 18:12:29 foot everforest soft

--- nixvim/ui/colorscheme/everforest/soft.nix
+++ nixvim/ui/colorscheme/everforest/soft.nix
@@ -1,12 +1,13 @@
 { lib, config, ... }: {
   # config = lib.mkIf (config.theme == "everforest_soft") {
+  # TODO change visual bg to gray1 or something
 
     programs.nixvim.colorschemes.everforest = {
       enable = true;
       settings = {
         background = "soft";
         disable_italic_comments = true;
-        show_eob = false; # ~ at end of buffer
+        show_eob = 0; # ~ at end of buffer
       };
     };
   # };
