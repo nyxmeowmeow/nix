bb470ab11bceca90d09334b63bbe6ffde5100f3a
rainmeower <rraaiin@proton.me>
2025-06-11 17:39:48 +1000

    11/06 17:39:48 add everforest

---
+++ themes/everforest/medium/colors.nix
@@ -0,0 +1,26 @@
+{
+  bg_dim    = "#232A2E";
+  bg        = "#2D353B";
+  bg1       = "#343F44";
+  bg2       = "#3D484D";
+  bg3       = "#475258";
+  bg4       = "#4F585E";
+  bg5       = "#56635F";
+  bg_red    = "#543A48";
+  bg_visual = "#514045";
+  bg_yellow = "#4D4C43";
+  bg_green  = "#425047";
+  bg_blue   = "#3A515D";
+  gray0     = "#7A8478";
+  gray1     = "#859289";
+  gray2     = "#9DA9A0";
+
+  fg     = "#D3C6AA";
+  red    = "#E67E80";
+  orange = "#E69875";
+  yellow = "#DBBC7F";
+  green  = "#A7C080";
+  blue   = "#7FBBB3";
+  aqua   = "#83C092";
+  purple = "#D699B6";
+}
---
+++ themes/everforest/soft/colors.nix
@@ -0,0 +1,26 @@
+{
+  bg_dim    = "#293136";
+  bg        = "#333C43";
+  bg1       = "#3A464C";
+  bg2       = "#434F55";
+  bg3       = "#4D5960";
+  bg4       = "#555F66";
+  bg5       = "#5D6B66";
+  bg_red    = "#5C3F4F";
+  bg_visual = "#59464C";
+  bg_yellow = "#55544A";
+  bg_green  = "#48584E";
+  bg_blue   = "#3F5865";
+  gray0     = "#7A8478";
+  gray1     = "#859289";
+  gray2     = "#9DA9A0";
+
+  fg     = "#D3C6AA";
+  red    = "#E67E80";
+  orange = "#E69875";
+  yellow = "#DBBC7F";
+  green  = "#A7C080";
+  blue   = "#7FBBB3";
+  aqua   = "#83C092";
+  purple = "#D699B6";
+}
