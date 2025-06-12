4213d2b83d7fddf81eb3dbafaeea6251d2c4d52e
rainmeower <rraaiin@proton.me>
2025-06-13 03:27:10 +1000

    13/06 03:27:10

--- nixvim/ui/colorizer.nix
+++ nixvim/ui/colorizer.nix
@@ -1,3 +1,15 @@
 {
-  programs.nixvim.plugins.colorizer.enable = true;
+programs.nixvim.plugins.colorizer = {
+  enable = true;
+    settings = {
+	RGB      = true;
+	RRGGBB   = true;
+	names    = false;
+	RRGGBBAA = true;
+	rgb_fn   = true;
+	hsl_fn   = true;
+	css      = true;
+	css_fn   = true;  
+    };
+  };
 }
