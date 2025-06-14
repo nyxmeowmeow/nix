11add170b701e7acb3c4fb5de66c800cfff427c5
rainmeower <rraaiin@proton.me>
2025-06-14 22:48:04 +1000

    14/06 22:48:04

--- nixvim/utils/surround.nix
+++ nixvim/utils/surround.nix
@@ -1,20 +1,5 @@
 {
   programs.nixvim.plugins.mini.modules.surround = {
     enable = true;
-    mappings = {
-      add = "sa";
-      delete = "sd";
-      find = "sf";
-      find_left = "sF";
-      highlight = "sh";
-      update_n_lines = "sn";
-
-      suffix_last = "l";
-      suffix_next = "n";
-    };
-    extraConfigLua = ''
-require('mini.surround').setup()
-
-    '';
   };
 }
