a6a5bdaf43966672af5d0a12f858e26880fa1d03
rainmeower <rraaiin@proton.me>
2025-06-14 12:22:45 +1000

    14/06 12:22:45

--- home_modules/yazi.nix
+++ home_modules/yazi.nix
@@ -7,9 +7,6 @@
     initLua = ''
     '';
 
-    plugins = {
-      compress = "https://github.com/KKV9/compress.yazi";
-    };
 
       # require("git"):setup()
       # require("folder-rules"):setup()
@@ -43,7 +40,7 @@
         { on = "I"; run = "forward"; desc = "Go forward to the next directory"; }
 
         # Toggle
-        # { on = "<Space>"; run = [ "toggle"; "arrow 1" ]; desc = "Toggle the current selection state"; }
+        { on = [ "r" "i" ]; run = [ "toggle" "arrow 1" ]; desc = "Toggle the current selection state"; }
         { on = "n"; run = [ "toggle" "arrow 1" ]; desc = "Toggle the current selection state"; }
         { on = "<C-g>";   run = "toggle_all --state=on"; desc = "Select all files"; }
         { on = "<C-v>";   run = "toggle_all";            desc = "Invert selection of all files"; }
@@ -66,17 +63,15 @@
         { on = [ "~" ]; run = "plugin simple-tag -- toggle-tag"; desc = "Toggle tag with any key (press any key to toggle tag)"; }
 
         # Operation
-        { on = "d";         run = "open";                        desc = "Open selected files"; }
-        { on = "D";         run = "open --interactive";          desc = "Open selected files interactively"; }
         { on = "<Enter>";   run = "open";                        desc = "Open selected files"; }
         { on = "<S-Enter>"; run = "open --interactive";          desc = "Open selected files interactively"; }
-        { on = "j";         run = [ "shell -- for path in '$@'; do echo 'file://$path'; done | wl-copy -t text/uri-list" "yank" ];   desc = "Yank selected files (copy)"; }
-        { on = "u";         run = "yank --cut";                  desc = "Yank selected files (cut)"; }
-        { on = "f";         run = "paste";                       desc = "Paste yanked files"; }
+        { on = "j";         run = [ "shell -- for path in '$@'; do echo 'file://$path'; done | wl-copy -t text/uri-list" "yank" ];   desc = "Yank"; }
+        { on = "u";         run = "yank --cut";                  desc = "Cut"; }
+        { on = "f";         run = "paste";                       desc = "Paste"; }
         # { on = "f"         run = "paste --force";               desc = "Paste yanked files (overwrite if the destination exists)"; }
-        { on = [ "<space>" "s" "l" ];         run = "link";                        desc = "Symlink absolute"; }
-        { on = [ "<space>" "s" "r" ];         run = "link --relative";             desc = "Symlink relative"; }
-        { on = [ "<space>" "s" "h" ];     run = "hardlink";                    desc = "Hardlink"; }
+        { on = [ "r" "s" "l" ];         run = "link";                        desc = "Symlink absolute"; }
+        { on = [ "r" "s" "r" ];         run = "link --relative";             desc = "Symlink relative"; }
+        { on = [ "r" "s" "h" ];     run = "hardlink";                    desc = "Hardlink"; }
         { on = "X";         run = "unyank";                      desc = "Cancel the yank status"; }
         { on = "o";         run = "remove";                      desc = "Trash selected files"; }
         { on = "t";         run = "create";                      desc = "Create a file (ends with / for directories)"; }
@@ -90,7 +85,7 @@
         { on = "b";         run = "plugin zoxide";               desc = "Jump to a directory via zoxide"; }
         { on = "B";         run = "plugin fzf";                  desc = "Jump to a file/directory via fzf"; }
 
-        { on   = ["<space>" "f"];       run  = "shell 'fish' --block";       desc = "Open shell here"; }
+        { on   = ["r" "f"];       run  = "shell 'fish' --block";       desc = "Open shell here"; }
 
         # Linemode
         { on = [ "m" "s" ]; run = "linemode size";        desc = "Linemode: size"; }
@@ -131,27 +126,27 @@
         #{ on = [ ";" "r" ]; run = "sort random --reverse=no";                      desc = "Sort randomly"; }
 
         # Goto
-        { on = [ "c" "<Space>" ]; run = "cd --interactive"; desc = "Jump interactively"; }
-        { on = [ "c" "r" ];       run = "cd /";             desc = "Goto root"; }
-        { on = [ "c" "h" ];       run = "cd ~";             desc = "Goto home"; }
-        { on = [ "c" "n" ];       run = "cd ~/nix";         desc = "Goto ~/nix"; }
-        { on = [ "c" "f" ];       run = "cd ~/.config";     desc = "Goto ~/.config"; }
-        { on = [ "c" "l" ];       run = "cd ~/.local";      desc = "Goto ~/.local"; }
-        { on = [ "c" "a" ];       run = "cd ~/art";         desc = "Goto ~/art"; }
-        { on = [ "c" "d" "e" ];  run = "cd ~/Desktop";     desc = "Goto ~/Desktop"; }
-        { on = [ "c" "d" "o" ];  run = "cd ~/Documents";   desc = "Goto ~/Documents"; }
-        { on = [ "c" "d" "l" ];  run = "cd ~/Downloads";   desc = "Goto ~/Downloads"; }
-        { on = [ "c" "m" "u" ];  run = "cd ~/Music";       desc = "Goto ~/Music"; }
-        { on = [ "c" "m" "i" ];  run = "cd ~/misc";       desc = "Goto ~/misc"; }
-        { on = [ "c" "i" ];       run = "cd ~/Pictures";    desc = "Goto ~/Pictures"; }
-        { on = [ "c" "t" ];       run = "cd ~/.local/share/Trash/files";    desc = "Goto Trash"; }
-        { on = [ "c" "p" ];       run = "cd ~/projects";    desc = "Goto ~/projects"; }
-        { on = [ "c" "v" ];       run = "cd ~/Videos";      desc = "Goto ~/Videos"; }
-        { on = [ "c" "o" "r" ];  run = "cd ~/Documents/Obsidian_Vault/Rust/Book_notes";      desc = "Goto obsidian rust"; }
-        { on = [ "c" "o" "n" ];  run = "cd ~/Documents/Obsidian_Vault/NixOS";      desc = "Goto obsidian nixos"; }
-        { on = [ "c" "s" "t" ];  run = "cd ~/.local/share/Steam/steamapps/common"; desc = "Goto steamapps"; }
-        { on = [ "c" "s" "i" ];  run = "cd ~/silly";       desc = "Goto silly"; }
-        { on = [ "c" "g" ];       run = "cd ~/.local/share/Steam/steamapps/compatdata/322170/pfx/drive_c/users/steamuser/AppData/Local/GeometryDash/"; desc = "Goto GD songs"; }
+        { on = [ "r" "<Space>" ]; run = "cd --interactive"; desc = "Jump interactively"; }
+        { on = [ "r" "r" ];       run = "cd /";             desc = "root"; }
+        { on = [ "r" "h" ];       run = "cd ~";             desc = "home"; }
+        { on = [ "r" "n" ];       run = "cd ~/nix";         desc = "~/nix"; }
+        { on = [ "r" "f" ];       run = "cd ~/.config";     desc = "~/.config"; }
+        { on = [ "r" "l" ];       run = "cd ~/.local";      desc = "~/.local"; }
+        { on = [ "r" "a" ];       run = "cd ~/art";         desc = "~/art"; }
+        { on = [ "r" "d" "e" ];  run = "cd ~/Desktop";     desc = "~/Desktop"; }
+        { on = [ "r" "d" "o" ];  run = "cd ~/Documents";   desc = "~/Documents"; }
+        { on = [ "r" "d" "l" ];  run = "cd ~/Downloads";   desc = "~/Downloads"; }
+        { on = [ "r" "m" "u" ];  run = "cd ~/Music";       desc = "~/Music"; }
+        { on = [ "r" "m" "i" ];  run = "cd ~/misc";       desc = "~/misc"; }
+        { on = [ "r" "i" ];       run = "cd ~/Pictures";    desc = "~/Pictures"; }
+        { on = [ "r" "t" ];       run = "cd ~/.local/share/Trash/files";    desc = "Trash"; }
+        { on = [ "r" "p" ];       run = "cd ~/projects";    desc = "~/projects"; }
+        { on = [ "r" "v" ];       run = "cd ~/Videos";      desc = "~/Videos"; }
+        { on = [ "r" "o" "r" ];  run = "cd ~/Documents/Obsidian_Vault/Rust/Book_notes";      desc = "obsidian rust"; }
+        { on = [ "r" "o" "n" ];  run = "cd ~/Documents/Obsidian_Vault/NixOS";      desc = "obsidian nix"; }
+        { on = [ "r" "s" "t" ];  run = "cd ~/.local/share/Steam/steamapps/common"; desc = "steamapps"; }
+        { on = [ "r" "s" "i" ];  run = "cd ~/silly";       desc = "silly"; }
+        { on = [ "r" "g" ];       run = "cd ~/.local/share/Steam/steamapps/compatdata/322170/pfx/drive_c/users/steamuser/AppData/Local/GeometryDash/"; desc = "GD songs"; }
 
 
         # Tabs
@@ -175,10 +170,6 @@
 
         # Tasks
         { on = "u"; run = "tasks_show"; desc = "Show task manager"; }
-
-        # Help
-        { on = "~";    run = "help"; desc = "Open help"; }
-        { on = "<F1>"; run = "help"; desc = "Open help"; }
 
 
         # Projects
