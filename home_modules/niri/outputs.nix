50c14d4c008500bc3919d1222352c4979c554ff9
rainmeower <rraaiin@proton.me>
2025-06-18 12:50:22 +1000

    18/06 12:50:22

--- home_modules/niri/outputs.nix
+++ home_modules/niri/outputs.nix
@@ -12,6 +12,18 @@
           refresh = 165.08;
         };
       };
+      "DP-3" = {
+        enable = true;
+        name = "secondary";
+        focus-at-startup = false;
+        variable-refresh-rate = false;
+        transform.rotation = 90;
+        mode = {
+          width = 2560;
+          height = 1440;
+          # refresh = 75;
+        };
+      };
     };
   };
 }
