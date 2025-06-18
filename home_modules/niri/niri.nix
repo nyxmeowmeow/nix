eacb3228162465a456a1a7f5a48ef0f1a61b92c0
rainmeower <rraaiin@proton.me>
2025-06-19 06:25:13 +1000

    19/06 06:25:13

--- home_modules/niri/niri.nix
+++ home_modules/niri/niri.nix
@@ -1,4 +1,39 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, inputs, ... }: 
+
+
+let
+  # Overlay to override wlr-which-key
+  overrides = self: super: {
+    wlr-which-key = super.rustPlatform.buildRustPackage rec {
+      pname = "wlr-which-key";
+      version = "master";
+
+      src = super.fetchFromGitHub {
+        owner = "MaxVerevkin";
+        repo = "wlr-which-key";
+        rev = "master";
+        sha256 = "<SHA256‑FOR‑SRC‑HERE>"; # fill this after first build
+      };
+
+      cargoSha256 = "<CARGO‑SHARED‑HERE>"; # get this too
+
+      # inherit other build dependencies (pkgs provides these by default)
+      nativeBuildInputs = super.rustPlatform.buildRustPackage.nativeBuildInputs;
+      buildInputs = super.rustPlatform.buildRustPackage.buildInputs;
+      propagatedBuildInputs = super.rustPlatform.buildRustPackage.propagatedBuildInputs;
+
+      meta = with super.stdenv.lib; {
+        description = "Keymap manager for wlroots-based compositors (from master)";
+        license = licenses.gpl3Only;
+        homepage = "https://github.com/MaxVerevkin/wlr-which-key";
+      };
+    };
+  };
+
+  myPkgs = import <nixpkgs> {
+    overlays = [ overrides ];
+  };
+in{
 
   imports = [
     inputs.niri.homeModules.niri 
@@ -13,11 +48,7 @@
 
   home.packages = with pkgs; [
     xwayland-satellite
-    # wlr-which-key = pks.wlr-which-key.overrideAttrs (old: rec {
-    # pname = "wlr-which-key";
-    # version = ol
-    #
-    # })
+    wlr-which-key
 
     # xdg-desktop-portal-gtk
     # xdg-desktop-portal-gnome
