3795352a32f99eb2fa03fefe78ad5872c5a623c5
rainmeower <rraaiin@proton.me>
2025-06-18 12:55:22 +1000

    18/06 12:55:22

--- home_modules/niri/outputs.nix
+++ home_modules/niri/outputs.nix
@@ -3,21 +3,25 @@
     outputs = {
       "DP-1" = {
         enable = true;
-        name = "main";
+
         focus-at-startup = true;
         variable-refresh-rate = false;
+
         mode = {
           width = 2560;
           height = 1440;
           refresh = 165.08;
         };
       };
+
       "DP-3" = {
         enable = true;
-        name = "secondary";
+
         focus-at-startup = false;
         variable-refresh-rate = false;
+
         transform.rotation = 90;
+
         mode = {
           width = 2560;
           height = 1440;
