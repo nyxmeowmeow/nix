b71fe0d8213825ea4022b263c6a25678dd9136a0
rainmeower <rraaiin@proton.me>
2025-06-18 14:29:28 +1000

    18/06 14:29:28

--- home_modules/niri/settings.nix
+++ home_modules/niri/settings.nix
@@ -1,9 +1,38 @@
 {
   programs.niri.settings = {
     prefer-no-csd = true;
-screenshot-path = "~/Pictures/Screenshots/%d/%m/%Y_%H-%M-%S.png";
-    hotkey-overlay = {
-    skip-at-startup = true;
+    screenshot-path = "~/Pictures/Screenshots/%d/%m/%Y_%H-%M-%S.png";
+    hotkey-overlay.skip-at-startup = true;
+
+    workspaces = {
+      "1" = {
+        name = "one";
+        open-on-output = "DP-1";
+      };
+      "2" = {
+        name = "browser";
+        open-on-output = "DP-1";
+      };
+      "7" = {
+        name = "game";
+        open-on-output = "DP-1";
+      };
+      "8" = {
+        name = "discord";
+        open-on-output = "DP-1";
+      };
+      "3" = {
+        name = "three";
+        open-on-output = "DP-1";
+      };
+      "4" = {
+        name = "browser_2";
+        open-on-output = "DP-1";
+      };
+      "9" = {
+        name = "alt";
+        open-on-output = "DP-3";
+      };
     };
 
   };
