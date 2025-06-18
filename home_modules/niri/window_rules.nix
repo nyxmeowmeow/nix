b8074ade8ccd4d97f10ed8dfdc916652beb7bf7b
rainmeower <rraaiin@proton.me>
2025-06-18 15:48:28 +1000

    18/06 15:48:28

---
+++ home_modules/niri/window_rules.nix
@@ -0,0 +1,34 @@
+{
+  programs.niri.settings.window-rules = [
+  {
+    draw-border-with-background = false;
+    geometry-corner-radius =
+      let
+        r = 10.0;
+      in
+      {
+        top-left = r;
+        top-right = r;
+        bottom-left = r;
+        bottom-right = r;
+      };
+    clip-to-geometry = true;
+  }
+  {
+    matches = [ { app-id = "^vesktop$"; } ];
+    block-out-from = [ "screen-capture" "screencast" ];
+  }
+  {
+    matches = [ { title = "mail"; } ];
+    block-out-from = [ "screen-capture" "screencast" ];
+  }
+  {
+    matches = [ { title = "bitwarden"; } ];
+    block-out-from = [ "screen-capture" "screencast" ];
+  }
+  {
+    matches = [ { title = "password"; } ];
+    block-out-from = [ "screen-capture" "screencast" ];
+  }
+  ];
+}
