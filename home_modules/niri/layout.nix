e93102d73aaae2d1c90ade0dbf43958ff37fcff5
rainmeower <rraaiin@proton.me>
2025-06-18 13:28:05 +1000

    18/06 13:28:05

--- home_modules/niri/layout.nix
+++ home_modules/niri/layout.nix
@@ -2,7 +2,30 @@
   programs.niri.settings.layout = {
     gaps = 10;
     center-focused-column = "never";
+    # center-focused-column = "on-overflow";
     always-center-single-column = true;
+
+    default-column-width = { proportion = 1. / 2.; };
+    preset-column-widths = [
+    { proportion = 1. / 3.; }
+    { proportion = 1. / 2.; }
+    { proportion = 2. / 3.; }
+    ];
+    preset-window-heights = [
+    { proportion = 1. / 3.; }
+    { proportion = 1. / 2.; }
+    { proportion = 2. / 3.; }
+    ];
+
+    focus-ring = {
+      enable = true;
+      width = 2;
+    };
+
+    border.enable = false;
+    shadow.enable = false;
+
+    tab-indicator.hide-when-single-tab = true;
 
   };
 }
