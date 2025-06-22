ac6805375356262a186e0b5cbbb7e7c80f845a6b
rainmeower <rraaiin@proton.me>
2025-06-22 15:55:59 +1000

    22/06 15:55:59 mpc scripts

---
+++ stuff/scripts/playlist.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+current=$(mpc --port 6669 --format %title% current)
+mpc --port 6669 --format %title% playlist | while read -r line; do
+    if [[ "$line" == "$current" ]]; then
+        echo "> $line"
+    else
+        echo "  $line"
+    fi
+done
