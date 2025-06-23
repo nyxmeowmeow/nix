14339a1fe13a3bd3648d2616fbd67c8b8927cb18
rainmeower <rraaiin@proton.me>
2025-06-23 16:20:39 +1000

    23/06 16:20:39

--- modules/nixos/core/configuration.nix
+++ modules/nixos/core/configuration.nix
@@ -20,7 +20,7 @@
   };
   environment.variables = {
     DISPLAY = ":0";
-      FZF_DEFAULT_OPTS = "--color=fg:#cad3f5,hl:#8bd5ca,fg+:#000000,bg+:#b7bdf8,hl+:#8bd5ca,info:#7f8c8d,prompt:#b7bdf8,spinner:-1,pointer:#b7bdf8,gutter:-1,info:#939ab7,border:-1 --border=none --info=hidden --header='' --prompt='󰘧 ' --no-bold -i";
+      FZF_DEFAULT_OPTS = "--color=fg:#cad3f5,hl:#8bd5ca,fg+:#000000,bg+:#b7bdf8,hl+:#8bd5ca,info:#7f8c8d,prompt:#b7bdf8,spinner:-1,pointer:#b7bdf8,gutter:-1,info:#939ab7,border:-1 --border=none --info=hidden --header='' --prompt='󰘧 ' --no-bold -i --pointer=''";
   };
 
   xdg.portal.enable = true;
