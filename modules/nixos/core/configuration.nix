9543943f4d7a1324e334821d6debe83a6b186735
rainmeower <rraaiin@proton.me>
2025-06-23 16:13:21 +1000

    23/06 16:13:21

--- modules/nixos/core/configuration.nix
+++ modules/nixos/core/configuration.nix
@@ -17,10 +17,10 @@
     NIXOS_OZONE_WL = "1"; # tell things to use wayland
     NIXPKGS_ALLOW_UNFREE = "1";
     DISPLAY = ":0";
-      FZF_DEFAULT_OPTS = "--color=fg:#cad3f5,hl:#8bd5ca,fg+:#cad3f5,bg+:#363a4f,hl+:#8bd5ca,info:#7f8c8d,prompt:#b7bdf8,spinner:-1,pointer:#b7bdf8,gutter:-1,info:#939ab7,border:-1 --border=none --info=hidden --header='' --prompt='󰘧 ' --no-bold -i";
   };
   environment.variables = {
     DISPLAY = ":0";
+      FZF_DEFAULT_OPTS = "--color=fg:#cad3f5,hl:#8bd5ca,fg+:#000000,bg+:#b7bdf8,hl+:#8bd5ca,info:#7f8c8d,prompt:#b7bdf8,spinner:-1,pointer:#b7bdf8,gutter:-1,info:#939ab7,border:-1 --border=none --info=hidden --header='' --prompt='󰘧 ' --no-bold -i";
   };
 
   xdg.portal.enable = true;
