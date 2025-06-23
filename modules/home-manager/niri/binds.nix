c69dda8e0989d1179aaaeff806571a170df69075
rainmeower <rraaiin@proton.me>
2025-06-23 16:25:19 +1000

    23/06 16:25:19

--- modules/home-manager/niri/binds.nix
+++ modules/home-manager/niri/binds.nix
@@ -9,8 +9,8 @@
     "Mod+L".action = sh "~/misc/wlr-which-key ~/.config/wlr-which-key/mpc.yaml";
 
     "Mod+T".action = spawn "footclient";
-    "Mod+C".action = spawn "footclient" "yazi";
-    "Mod+S".action = spawn "footclient" "nvim";
+    "Mod+C".action = sh "export FZF_DEFAULT_OPTS=\"--color=fg:#cad3f5,hl:#8bd5ca,fg+:#000000,bg+:#b7bdf8,hl+:#8bd5ca,info:#7f8c8d,prompt:#b7bdf8,spinner:-1,pointer:#b7bdf8,gutter:-1,info:#939ab7,border:-1 --border=none --info=hidden --header='' --prompt='󰘧 ' --no-bold -i --pointer=''\" footclient yazi";
+    "Mod+S".action = sh "footclient nvim";
 
 
     "XF86AudioRaiseVolume".action = sh "wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%+";
