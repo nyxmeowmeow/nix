0f97a5e5116441e1050fb6b0d513b357cfa6abb4
rainmeower <rraaiin@proton.me>
2025-06-23 16:11:29 +1000

    23/06 16:11:29

--- themes/black/fzf_black.nix
+++ themes/black/fzf_black.nix
@@ -1,7 +1,7 @@
 { lib, theme, ... }: {
-  config = lib.mkIf (theme == "black") {
-    home.sessionVariables = {
-      FZF_DEFAULT_OPTS = "--color=fg:#cad3f5,hl:#8bd5ca,fg+:#cad3f5,bg+:#363a4f,hl+:#8bd5ca,info:#7f8c8d,prompt:#b7bdf8,spinner:-1,pointer:#b7bdf8,gutter:-1,info:#939ab7,border:-1 --border=none --info=hidden --header='' --prompt='󰘧 ' --no-bold -i";
-    };
-  };
+  # config = lib.mkIf (theme == "black") {
+  #   home.sessionVariables = {
+  #     FZF_DEFAULT_OPTS = "--color=fg:#cad3f5,hl:#8bd5ca,fg+:#cad3f5,bg+:#363a4f,hl+:#8bd5ca,info:#7f8c8d,prompt:#b7bdf8,spinner:-1,pointer:#b7bdf8,gutter:-1,info:#939ab7,border:-1 --border=none --info=hidden --header='' --prompt='󰘧 ' --no-bold -i";
+  #   };
+  # };
 }
