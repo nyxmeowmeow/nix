180e99aaabf913f1d45035576780b77594e610f9
rainmeower <rraaiin@proton.me>
2025-06-25 19:23:50 +1000

    25/06 19:23:50

--- themes/lix/home-manager/yazi.nix
+++ themes/lix/home-manager/yazi.nix
@@ -1,6 +1,6 @@
 { lib, theme, ... }:
 let
-  col = import ../colors.nix;
+col = import ../colors.nix;
 in {
   config = lib.mkIf (theme == "lix") {
     programs.yazi.theme = {
@@ -31,13 +31,13 @@ in {
           bg = col.red;
         };
 
-        # visual select
+# visual select
         marker_marked = {
           fg = col.violet;
           bg = col.violet;
         };
 
-        # one at a time
+# one at a time
         marker_selected = {
           fg = col.blue;
           bg = col.blue;
@@ -80,6 +80,73 @@ in {
         perm_readf.fg = col.gold;
         perm_writef.fg = col.bubblegum;
         perm_execf.fg = col.mint;
+      };
+      filetype = {
+
+        rules = [
+# Images
+        {
+          mime = "image/*";
+          fg = col.gold;
+        }
+
+# Media
+        {
+          mime = "{audio,video}/*";
+          fg = col.violet;
+        }
+
+# Archives
+        {
+          mime = "application/{zip,rar,7z*,tar,gzip,xz,zstd,bzip*,lzma,compress,archive,cpio,arj,xar,ms-cab*}";
+          fg = col.bubblegum;
+        }
+
+# Documents
+        {
+          mime = "application/{pdf,doc,rtf}";
+          fg = col.blue;
+        }
+
+# Empty files
+# { mime = "inode/empty", fg = "red" },
+
+# Special files
+        {
+          name = "*";
+          is = "orphan";
+          bg = col.red;
+        }
+
+        {
+          name = "*";
+          is = "exec";
+          fg = col.mint;
+        }
+
+# Dummy files
+        { name = "*";
+          is = "dummy";
+          bg = col.red;
+        }
+        {
+          name = "*/"; is = "dummy";
+          bg = col.red;
+        }
+
+# Fallback
+        {
+          name = "*";
+          fg = col.fg;
+        }
+        {
+          name = "*/";
+          fg = col.pink;
+        }
+        ];
+
+
+
       };
     };
   };
