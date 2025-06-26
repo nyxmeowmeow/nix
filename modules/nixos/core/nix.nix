b1e82d57c4f25684fef8329ff6063544a75d063f
rainmeower <rraaiin@proton.me>
2025-06-27 06:47:06 +1000

    27/06 06:47:06

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -2,9 +2,14 @@
   nix = {
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
-      auto-optimise-store = true;
+      # auto-optimise-store = true; # optimise on every rebuild
       keep-derivations = false;
       keep-outputs = false;
+    };
+
+    optimise = {
+      automatic = true;
+      dates = [ "00:00" ];
     };
 
     gc = {
