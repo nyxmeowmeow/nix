876b52bd8d4037adbb874bee23b7534faa4c98e6
rainmeower <rraaiin@proton.me>
2025-06-27 03:48:48 +1000

    27/06 03:48:48

--- overlays/nvim.nix
+++ overlays/nvim.nix
@@ -1,9 +1,8 @@
 self: super: {
   neovim = super.neovim.overrideAttrs (old: {
-    # Add CFLAGS for C compiler via cmake
     cmakeFlags = (old.cmakeFlags or []) ++ [
-      "-DCMAKE_C_FLAGS=-Ofast -march=native -pipe"
-      "-DCMAKE_CXX_FLAGS=-Ofast -march=native -pipe"
+      "-DCMAKE_C_FLAGS=-O3 -march=native -pipe"
+      "-DCMAKE_CXX_FLAGS=-O3 -march=native -pipe"
     ];
   });
 }
