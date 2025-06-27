cc8ad5f7d425bf7654caf406d1f0e61345e3e80b
rainmeower <rraaiin@proton.me>
2025-06-28 08:47:58 +1000

    28/06 08:47:58

--- modules/home-manager/nixvim/utils/treesitter-textobjects.nix
+++ modules/home-manager/nixvim/utils/treesitter-textobjects.nix
@@ -4,21 +4,21 @@
 
     select = {
       enable = true;
-      # lookahead = true;
-      # keymaps = {
-      #   "aa" = "@parameter.outer";
-      #   "ia" = "@parameter.inner";
-      #   "af" = "@function.outer";
-      #   "if" = "@function.inner";
-      #   "ac" = "@class.outer";
-      #   "ic" = "@class.inner";
-      #   "as" = "@local.scope";
-      #   "ii" = "@conditional.inner";
-      #   "ai" = "@conditional.outer";
-      #   "il" = "@loop.inner";
-      #   "al" = "@loop.outer";
-      #   "at" = "@comment.outer";
-      # };
+      lookahead = true;
+      keymaps = {
+        "aa" = "@parameter.outer";
+        "ia" = "@parameter.inner";
+        "af" = "@function.outer";
+        "if" = "@function.inner";
+        "ac" = "@class.outer";
+        "ic" = "@class.inner";
+        "as" = "@local.scope";
+        "ii" = "@conditional.inner";
+        "ai" = "@conditional.outer";
+        "il" = "@loop.inner";
+        "al" = "@loop.outer";
+        "at" = "@comment.outer";
+      };
     };
   };
 }
