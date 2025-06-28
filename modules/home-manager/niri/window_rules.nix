c257227d02f63a07e477732847620411c4e7209e
rainmeower <rraaiin@proton.me>
2025-06-29 02:03:47 +1000

    29/06 02:03:47

--- modules/home-manager/niri/window_rules.nix
+++ modules/home-manager/niri/window_rules.nix
@@ -16,16 +16,41 @@
       };
     clip-to-geometry = true;
   }
+
   {
     matches = [ { app-id = "^vesktop$"; } ];
-    block-out-from = "screencast";
+    block-out-from = "screen-capture";
   }
-  # {
-  #   matches = [ { title = "mail"; } ];
-  #   block-out-from = [ "screen-capture" "screencast" ];
-  # }
-  # {
-  #   matches = [ { title = "bitwarden"; } ];
+
+  {
+    matches = [ { 
+      app-id = "steam_app_2225070";
+      title = "Trackmania";
+    } ];
+    opacity = 0.0;
+    open-on-workspace = "game";
+    min-width = 2560;
+    max-width = 2560;
+    min-height = 1080;
+    max-height = 1080;
+    # geometry-corner-radius = 0;
+    variable-refresh-rate = false;
+  }
+
+  { # ubisoft connect little white window
+    matches = [ { 
+      app-id = "steam_app_2225070";
+      title = "";
+    } ];
+    opacity = 0.0;
+  }
+
+# {
+#   matches = [ { title = "mail"; } ];
+#   block-out-from = [ "screen-capture" "screencast" ];
+# }
+# {
+#   matches = [ { title = "bitwarden"; } ];
   #   block-out-from = [ "screen-capture" "screencast" ];
   # }
   # {
