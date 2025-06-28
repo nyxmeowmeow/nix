e9799d278f4d71426422fb8dff061c4a443adc99
rainmeower <rraaiin@proton.me>
2025-06-28 20:49:18 +1000

    28/06 20:49:18

--- stuff/scripts/hextoimg.sh
+++ stuff/scripts/hextoimg.sh
@@ -1,19 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env dash
 
-# Get the hex color code from hyprpicker
 COLOR_CODE=$(hyprpicker -a)
-
-# Remove any leading or trailing whitespace from COLOR_CODE
 COLOR_CODE=$(echo "$COLOR_CODE" | xargs)
-
-# Path to the Python script
 PYTHON_SCRIPT="/home/meow/nix/stuff/scripts/hextoimg.py"
-
-# Path to the output image
 IMAGE_PATH="/home/meow/.config/dunst/icons/hyprpicker/color_image.ppm"
-
-# Call the Python script with the color code
 python3 "$PYTHON_SCRIPT" "$COLOR_CODE"
-
-# Send a notification with the output image
 notify-send -a "t2" -i "$IMAGE_PATH" "$COLOR_CODE"
--- stuff/scripts/melee.sh
+++ stuff/scripts/melee.sh
@@ -2,9 +2,3 @@
 
 foot --hold sudo steam-run /home/$(whoami)/melee/keyb0xx &
 gamescope slippi-launcher
-
-# & 
-# sleep 0.1
-# hyprctl dispatch movetoworkspacesilent special:2
-# sleep 0.1
-# hyprctl dispatch workspace 8
