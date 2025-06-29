86969726e68f3816a1adf949b9a9739904d6d15d
rainmeower <rraaiin@proton.me>
2025-06-29 21:58:52 +1000

    29/06 21:58:52

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -13,6 +13,7 @@
         ../../themes/macchiato/home-manager/default.nix
         ../../themes/ocean/home-manager/default.nix
 
+        ./nh.nix
         ./navi.nix
         ./desktop.nix
         ./btop.nix
