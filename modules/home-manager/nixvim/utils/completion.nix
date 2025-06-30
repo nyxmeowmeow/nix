1569130aaf9365cedbb3895a4d7f7385e8e7c718
rainmeower <rraaiin@proton.me>
2025-06-30 10:38:12 +1000

    30/06 10:38:12

--- modules/home-manager/nixvim/utils/completion.nix
+++ modules/home-manager/nixvim/utils/completion.nix
@@ -1,26 +1,26 @@
 {
-programs.nixvim.plugins.cmp.enable = true;
+# programs.nixvim.plugins.cmp.enable = true;
 
-  # programs.nixvim.plugins.mini.modules.completion = {
-  #   enable = true;
-  #
-  #   settings = {
-  #     lspCompletion = {
-  #       sourceFunc = "omnifunc";
-  #       autoSetup = true;
-  #     };
-  #     window = {
-  #       info = {
-  #         height = 20;
-  #         width = 30;
-  #         border = null;
-  #       };
-  #       signature = {
-  #         height = 25;
-  #         width = 80;
-  #         border = null;
-  #       };
-  #     };
-  #   };
-  # };
+  programs.nixvim.plugins.mini.modules.completion = {
+    enable = true;
+
+    settings = {
+      lspCompletion = {
+        sourceFunc = "completefunc";
+        autoSetup = true;
+      };
+      window = {
+        info = {
+          height = 20;
+          width = 30;
+          border = null;
+        };
+        signature = {
+          height = 25;
+          width = 80;
+          border = null;
+        };
+      };
+    };
+  };
 }
