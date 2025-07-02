c706abaabd6c6b122ac4bbeb7180c0dc177190a1
rainmeower <rraaiin@proton.me>
2025-07-03 08:55:25 +1000

    03/07 08:59:42 blacklist bluetooth, battery, vivaldi mod and move amdgpu mod further into boot process

--- modules/nixos/core/boot.nix
+++ modules/nixos/core/boot.nix
@@ -7,11 +7,18 @@
       systemd-boot.configurationLimit = 50;
       efi.canTouchEfiVariables = true;
     };
-    initrd.kernelModules = [ "amdgpu" ];
+    # initrd.kernelModules = [ "amdgpu" ];
+    kernelModules = [ "amdgpu" ];
     kernelPackages = pkgs.linuxPackages_xanmod_latest; # pkgs.linuxPackages_zen;
     kernelParams = [
       "video=DP-1:2560x1440@165"
       "video=DP-3:2560x1440@75"
+    ];
+    blacklistedKernelModules = [
+      "bluetooth"
+      "vivaldi_fmap"
+      "battery"
+
     ];
   };
 }
