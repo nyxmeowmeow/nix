f34d783759ca4786aa0e654ba4cee87892e77097
rainmeower <rraaiin@proton.me>
2025-07-03 09:07:27 +1000

    03/07 09:07:32 actually blacklist bluetooth

--- modules/nixos/core/boot.nix
+++ modules/nixos/core/boot.nix
@@ -7,18 +7,25 @@
       systemd-boot.configurationLimit = 50;
       efi.canTouchEfiVariables = true;
     };
-    # initrd.kernelModules = [ "amdgpu" ];
-    kernelModules = [ "amdgpu" ];
+
     kernelPackages = pkgs.linuxPackages_xanmod_latest; # pkgs.linuxPackages_zen;
     kernelParams = [
       "video=DP-1:2560x1440@165"
       "video=DP-3:2560x1440@75"
     ];
+
+    # initrd.kernelModules = [];
+    kernelModules = [ "amdgpu" ];
     blacklistedKernelModules = [
       "bluetooth"
+      "btrtl"
+      "btmtk"
+      "btintel"
+      "btbcm"
+      "btusb"
+
       "vivaldi_fmap"
       "battery"
-
     ];
   };
 }
