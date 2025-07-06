4f8512112ccd7c8732e82e2835fdcd1ed55bbb28
rainmeower <rraaiin@proton.me>
2025-07-06 13:58:01 +1000

    06/07 13:58:01

--- modules/nixos/font.nix
+++ modules/nixos/font.nix
@@ -1,17 +1,17 @@
 { lib, config, pkgs, ... }: {
-  config = lib.mkMerge [
-    (lib.mkIf (config.font == "mononoki") {
+  # config = lib.mkMerge [
+  #   (lib.mkIf (config.font == "mononoki") {
       stylix.fonts.monospace = {
         package = pkgs.nerd-fonts.mononoki;
         name = "Mononoki Nerd Font Mono";
       };
-    })
+    # })
 
-    (lib.mkIf (config.font == "iosevka") {
-      stylix.fonts.monospace = {
-        package = pkgs.nerd-fonts.iosevka;
-        name = "Iosevka Nerd Font Mono";
-      };
-    })
-  ];
+  #   (lib.mkIf (config.font == "iosevka") {
+  #     stylix.fonts.monospace = {
+  #       package = pkgs.nerd-fonts.iosevka;
+  #       name = "Iosevka Nerd Font Mono";
+  #     };
+  #   })
+  # ];
 }
