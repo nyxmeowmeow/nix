6f6da8b6e480f14263498cfe59ead661aa43985f
rainmeower <rraaiin@proton.me>
2025-07-07 18:52:54 +1000

    07/07 18:52:54

--- modules/home-manager/tui/nixvim/utils/completion.nix
+++ modules/home-manager/tui/nixvim/utils/completion.nix
@@ -13,21 +13,21 @@ programs.nixvim.plugins.cmp = {
       "cmp.PreselectMode.None";
     performance.max_view_entries = 5;
     mapping = {
-      "<C-j>" = # lua
-        "cmp.mapping.scroll_docs(-4)";
-      "<C-k>" = # lua
-        "cmp.mapping.scroll_docs(4)";
+      # "<C-j>" = # lua
+      #   "cmp.mapping.scroll_docs(-4)";
+      # "<C-k>" = # lua
+      #   "cmp.mapping.scroll_docs(4)";
       # "<C-Space>" = # lua
         # "cmp.mapping.complete()";
       # "<C-h>" = # lua
       #   "cmp.mapping.close()";
       # "<Tab>" = # lua
       #   "cmp.mapping(cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})";
+      # "<C-i>" = # lua
+      #   "cmp.mapping(cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})  ";
+      # "<C-u>" = # lua
+      #   "cmp.mapping(cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})";
       "<C-i>" = # lua
-        "cmp.mapping(cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})  ";
-      "<C-u>" = # lua
-        "cmp.mapping(cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})";
-      "<C-h>" = # lua
         "cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })";
     };
 
