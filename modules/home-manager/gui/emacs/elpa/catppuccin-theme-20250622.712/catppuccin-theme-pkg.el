9f9e2e6357e434428157bb9c865368d724d15bb2
rainmeower <rraaiin@proton.me>
2025-07-20 21:38:46 +1000

    20/07 21:38:46

--- modules/home-manager/gui/emacs/elpa/catppuccin-theme-20250622.712/catppuccin-theme-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "catppuccin-theme" "20250622.712"
-  "Catppuccin for Emacs - 🍄 Soothing pastel theme for Emacs."
-  '((emacs "26.1"))
-  :url "https://github.com/catppuccin/emacs"
-  :commit "26766bbc61d59a94088aaa4cccbb9d39b5a8b5b8"
-  :revdesc "26766bbc61d5"
-  :maintainers '(("Jeremy Baxter" . "jeremy@baxters.nz")))
