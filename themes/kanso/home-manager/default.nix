d129df47fe382aaa5d4fe808e7010a2ef3531946
nyx-meow <rraaiin@proton.me>
2025-07-26 16:58:49 +1000

    26/07 16:58:49

--- themes/kanso/home-manager/default.nix
+++ themes/kanso/home-manager/default.nix
@@ -4,7 +4,9 @@
     ./gtk_kanso.nix
     ./fastfetch_kanso.nix
     ./hyprland_kanso.nix
-    ./foot_kanso.nix
+    ./foot_kanso_mist.nix
+    ./foot_kanso_ink.nix
+    ./foot_kanso_zen.nix
     ./hyprpaper_kanso.nix
     ./starship_kanso.nix
     ./nushell_kanso.nix
