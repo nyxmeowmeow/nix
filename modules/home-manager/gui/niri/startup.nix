5a65debd7700ace2e425c623224a0cc6e8b521ee
nyx-meow <rraaiin@proton.me>
2025-07-26 15:50:20 +1000

    26/07 15:50:20

--- modules/home-manager/gui/niri/startup.nix
+++ modules/home-manager/gui/niri/startup.nix
@@ -1,11 +1,11 @@
 {
   programs.niri.settings.spawn-at-startup = [
-    { command = [ "kitty" "-1" ]; }
-    { command = [ "kitty" "-1" "xwayland-satellite" ]; }
-    { command = [ "kitty" "-1" "~/.config/kanata/hyprkan -d" ]; }
-    # { command = [ "foot" "--server" ]; }
-    # { command = [ "foot" "-H" "xwayland-satellite" ]; }
-    # { command = [ "foot" "-H" "~/.config/kanata/hyprkan -d" ]; }
+    # { command = [ "kitty" "-1" ]; }
+    # { command = [ "kitty" "-1" "xwayland-satellite" ]; }
+    # { command = [ "kitty" "-1" "~/.config/kanata/hyprkan -d" ]; }
+    { command = [ "foot" "--server" ]; }
+    { command = [ "foot" "-H" "xwayland-satellite" ]; }
+    { command = [ "foot" "-H" "~/.config/kanata/hyprkan -d" ]; }
 
     { command = [ "clipse" "-listen" ]; }
     { command = [ "udiskie" "-a" ]; }
