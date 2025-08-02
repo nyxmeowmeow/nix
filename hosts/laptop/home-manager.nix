13e8c72eaac6b599ff15699f7b37118c0de90e07
nyx-meow <rraaiin@proton.me>
2025-08-03 08:09:58 +1000

    03/08 08:09:58

--- hosts/laptop/home-manager.nix
+++ hosts/laptop/home-manager.nix
@@ -23,30 +23,33 @@
         inputs.nixvim.homeModules.nixvim
         ../../modules/home-manager/tui/nixvim/default.nix
 
-        ../../modules/home-manager/tui/nh.nix
-        ../../modules/home-manager/tui/navi.nix
-        ../../modules/home-manager/tui/btop.nix
-        ../../modules/home-manager/tui/git.nix
-#        ../../modules/home-manager/gui/obs.nix
-        ../../modules/home-manager/tui/clipse.nix
+
+
+        # ./hyprland.nix
         ../../modules/home-manager/gui/dunst.nix
-#        ../../modules/home-manager/gui/emacs/emacs.nix
         ../../modules/home-manager/gui/foot.nix
-        ../../modules/home-manager/tui/fzf.nix
-        ../../modules/home-manager/tui/git.nix
-#        ../../modules/home-manager/gui/mpv.nix
-#        ../../modules/home-manager/tui/ncmpcpp.nix
         ../../modules/home-manager/gui/niri/niri.nix
-        ./niri.nix
-        ../../modules/home-manager/tui/nushell.nix
-        ../../modules/home-manager/tui/starship.nix
         ../../modules/home-manager/gui/tofi.nix
         ../../modules/home-manager/gui/vesktop.nix
-        ../../modules/home-manager/tui/yazi/yazi.nix
         ../../modules/home-manager/gui/zen/zen.nix
-        ../../modules/home-manager/tui/zoxide.nix
         ../../modules/home-manager/tui/bash.nix
-        # ./hyprland.nix
+        ../../modules/home-manager/tui/btop.nix
+        ../../modules/home-manager/tui/clipse.nix
+        ../../modules/home-manager/tui/fzf.nix
+        ../../modules/home-manager/tui/git.nix
+        ../../modules/home-manager/tui/git.nix
+        ../../modules/home-manager/tui/navi.nix
+        ../../modules/home-manager/tui/nh.nix
+        ../../modules/home-manager/tui/nushell.nix
+        ../../modules/home-manager/tui/starship.nix
+        ../../modules/home-manager/tui/yazi/yazi.nix
+        ../../modules/home-manager/tui/zellij/default.nix.nix
+        ../../modules/home-manager/tui/zoxide.nix
+        ./niri.nix
+#        ../../modules/home-manager/gui/emacs/emacs.nix
+#        ../../modules/home-manager/gui/mpv.nix
+#        ../../modules/home-manager/gui/obs.nix
+#        ../../modules/home-manager/tui/ncmpcpp.nix
       ];
     };
   };
