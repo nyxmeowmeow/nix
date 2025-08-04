410c39d46f31f39157b4b834bdb568b8d1b0112b
nyx-meow <rraaiin@proton.me>
2025-08-04 11:56:09 +1000

    04/08 11:56:09

--- modules/home-manager/services/easyeffects.nix
+++ modules/home-manager/services/easyeffects.nix
@@ -5,70 +5,73 @@
     extraPresets = {
     my-preset = {
       output = {
-        "equalizer#0" = {
+        equalizer = {
           output-gain = -20.0;
           band0 = {
             frequency = 105.0;
             gain = 2.0;
-            mode = "LSC";
+            # mode = "LSC";
             q = 0.7;
           };
           band1 = {
             frequency = 116.8;
             gain = -4.4;
-            mode = "PK";
+            # mode = "PK";
             q = 0.44;
           };
           band2 = {
             frequency = 194.8;
             gain = -1.4;
-            mode = "PK";
+            # mode = "PK";
             q = 1.3;
           };
           band3 = {
             frequency = 347.2;
             gain = 3.0;
-            mode = "PK";
+            # mode = "PK";
             q = 1.04;
           };
           band4 = {
             frequency = 933.1;
             gain = 1.1;
-            mode = "PK";
+            # mode = "PK";
             q = 0.59;
           };
           band5 = {
             frequency = 1771.8;
             gain = 1.5;
-            mode = "PK";
+            # mode = "PK";
             q = 2.33;
           };
           band6 = {
             frequency = 1785.9;
             gain = -1.6;
-            mode = "PK";
+            # mode = "PK";
             q = 2.69;
           };
           band7 = {
             frequency = 3858.0;
             gain = 3.7;
-            mode = "PK";
+            # mode = "PK";
             q = 6.0;
           };
           band8 = {
             frequency = 7780.3;
             gain = -3.0;
-            mode = "PK";
+            # mode = "PK";
             q = 4.13;
           };
           band9 = {
             frequency = 10000.0;
             gain = -7.0;
-            mode = "HSC";
+            # mode = "HSC";
             q = 0.7;
           };
         };
       };
+      plugins-order = [
+      "equalizer"
+      ];
     };
   };
   };
