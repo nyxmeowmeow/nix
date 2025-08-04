7edd21cf4e837032208c3e11687f6398b20b14f0
nyx-meow <rraaiin@proton.me>
2025-08-04 11:30:39 +1000

    04/08 11:30:39

--- modules/home-manager/services/easyeffects.nix
+++ modules/home-manager/services/easyeffects.nix
@@ -1,5 +1,18 @@
 {
-services.easyeffects = {
-enable = true;
-};
+  services.easyeffects = {
+    enable = true;
+    preset = ''
+      Preamp: -13.82 dB
+      Filter 1: ON LSC Fc 105.0 Hz Gain 2.0 dB Q 0.70
+      Filter 2: ON PK Fc 116.8 Hz Gain -4.4 dB Q 0.44
+      Filter 3: ON PK Fc 194.8 Hz Gain -1.4 dB Q 1.30
+      Filter 4: ON PK Fc 347.2 Hz Gain 3.0 dB Q 1.04
+      Filter 5: ON PK Fc 933.1 Hz Gain 1.1 dB Q 0.59
+      Filter 6: ON PK Fc 1771.8 Hz Gain 1.5 dB Q 2.33
+      Filter 7: ON PK Fc 1785.9 Hz Gain -1.6 dB Q 2.69
+      Filter 8: ON PK Fc 3858.0 Hz Gain 3.7 dB Q 6.00
+      Filter 9: ON PK Fc 7780.3 Hz Gain -3.0 dB Q 4.13
+      Filter 10: ON HSC Fc 10000.0 Hz Gain -7.0 dB Q 0.70
+    '';
+  };
 }
