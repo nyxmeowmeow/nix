16b0a2d6b5a320db07f9ab470c65d3f0e797fdc5
nyx-meow <rraaiin@proton.me>
2025-08-09 22:25:46 +1000

    09/08 22:25:46

--- modules/home-manager/gui/foot.nix
+++ modules/home-manager/gui/foot.nix
@@ -5,12 +5,9 @@
       main = {
         term = "foot";
         font = "${config.stylix.fonts.monospace.name}:size=26";
-        # font = "/home/meow/iosevka/Iosevka/dist/IosevkaCustom/TTF/IosevkaCustom-Regular.ttf:size=26";
-        # font = "Mononoki Nerd Font Mono:size=26";
         dpi-aware = false;
         shell = "nu";
         pad = "12x6";
-
         box-drawings-uses-font-glyphs = false;
       };
 
@@ -20,6 +17,7 @@
         unfocused-style = "unchanged";
       };
       # tweak.box-drawing-base-thickness = "0.04";
+
       key-bindings = {
         scrollback-up-half-page = "Control+Shift+e";
         scrollback-down-half-page = "Control+Shift+a";
--- themes/kanso/home-manager/niri_kanso.nix
+++ themes/kanso/home-manager/niri_kanso.nix
@@ -6,15 +6,12 @@ in {
 
     programs.niri.settings.layout = {
       focus-ring = {
-        enable = true;
-        width = 2;
-        active.color = col.blue;
+      active.color = col.blue;
+      inactive.color = col.blue;
       };
       tab-indicator = {
-        enable = true;
         active.color = col.blue;
         inactive.color = col.gray5;
-        hide-when-single-tab = false;
       };
     };
   };
