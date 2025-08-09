5f9539cd94249a8ee83245fe3f6b7e983a9f5e4b
nyx-meow <rraaiin@proton.me>
2025-08-09 17:58:25 +1000

    09/08 17:58:25

--- themes/kanso/home-manager/default.nix
+++ themes/kanso/home-manager/default.nix
@@ -1,14 +1,15 @@
 {
   imports = [
     ./btop_kanso.nix
-    ./niri_kanso.nix
     ./fastfetch_kanso.nix
-    ./foot_kanso_mist.nix
     ./foot_kanso_ink.nix
+    ./foot_kanso_mist.nix
     ./foot_kanso_zen.nix
     ./hyprpaper_kanso.nix
-    ./starship_kanso.nix
+    ./niri_kanso.nix
     ./nushell_kanso.nix
+    ./starship_kanso.nix
+    ./walker_kanso.nix
     ./yazi.nix
   ];
 }
