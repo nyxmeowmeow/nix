e79c6774d62e634c924a6f7cf61afc123731d763
nyx-meow <rraaiin@proton.me>
2025-08-17 13:00:25 +1000

    17/08 13:00:25 niri tablet

--- modules/home-manager/gui/niri/input.nix
+++ modules/home-manager/gui/niri/input.nix
@@ -1,10 +1,14 @@
 {
   programs.niri.settings = {
     input = {
-    focus-follows-mouse.enable = true;
+      focus-follows-mouse.enable = true;
       mouse = {
         accel-profile = "flat";
         accel-speed = 0.2;
+      };
+      tablet = {
+        enable = true;
+        map-to-output = "DP-1";
       };
     };
     cursor.hide-after-inactive-ms = 1000;
