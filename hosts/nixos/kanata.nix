81c877bb755ced16f32a97f31f7c863851d33396
nyx-meow <rraaiin@proton.me>
2025-08-27 01:46:44 +1000

    27/08 01:46:44

--- hosts/nixos/kanata.nix
+++ hosts/nixos/kanata.nix
@@ -45,7 +45,7 @@
   cw (caps-word-custom
     2000
     (a b c d e f g h i j k l m n o p q r s t u v w x y z)
-    (bspc del up down left rght - 0 1 2 3 4 5 6 7 8 9)
+    (bspc del up down left rght - 0 1 2 3 4 5 6 7 8 9 lsft rsft)
   )
  .tp (hold-for-duration 30 typingmode)
 
