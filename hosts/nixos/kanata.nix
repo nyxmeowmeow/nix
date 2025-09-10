dae5a6ddf4799cf09e9c6227e40bff4ca162301f
nyxmeowmeow <rraaiin@proton.me>
2025-09-10 18:02:06 +1000

    10/09 18:02:06

--- hosts/nixos/kanata.nix
+++ hosts/nixos/kanata.nix
@@ -229,7 +229,7 @@
   ;; FIXME
   rpeat (switch
 
-    ((key-history lsft 1)) (macro (unmod = =)) break
+    ((key-history lsft 1)) (macro = =) break
 
     ((key-history , 1)) (macro S-,) break
     ;; ((key-history . 1)) (macro S-.) break
