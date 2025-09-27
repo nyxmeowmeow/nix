b6e14c34d7f1becce07a222acaeb8330b8487151
nyxmeowmeow <rraaiin@proton.me>
2025-09-27 17:36:35 +1000

    27/09 17:36:35

--- themes/kanso/home-manager/hyprpaper_kanso.nix
+++ themes/kanso/home-manager/hyprpaper_kanso.nix
@@ -9,13 +9,12 @@
         splash_offset = 2.0;
 
         preload = [
-          # "${flake_dir}/stuff/wallpapers/clouds.jpg" 
-          "${flake_dir}/stuff/wallpapers/underwater.png" 
+          "${flake_dir}/stuff/wallpapers/rocks_blur.png" 
         ];
 
         wallpaper = [
-          "DP-1, ${flake_dir}/stuff/wallpapers/underwater.png"
-          "DP-3, ${flake_dir}/stuff/wallpapers/underwater.png"
+          "DP-1, ${flake_dir}/stuff/wallpapers/rocks_blur.png"
+          "DP-3, ${flake_dir}/stuff/wallpapers/rocks_blur.png"
         ];
       };
     };
