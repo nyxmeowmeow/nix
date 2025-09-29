17cac5c55aa3bc735bfed30ce0b23c95944d2e2e
nyxmeowmeow <rraaiin@proton.me>
2025-09-29 22:39:25 +1000

    29/09 22:39:25

--- modules/home-manager/tui/nixvim/utils/lsp.nix
+++ modules/home-manager/tui/nixvim/utils/lsp.nix
@@ -1,21 +1,22 @@
 {
-  programs.nixvim.plugins = {
-    lsp = {
-      enable = true;
-      servers = {
-        nixd.enable = true;
-        qmlls.enable = true;
-        hls = {
-          enable = true;
-          installGhc = false;
-        };
-        clangd.enable = true;
-        rust_analyzer = {
-          enable = true;
-          installRustc = false;
-          installCargo = false;
-        };
-      };
-    };
-  };
+  # programs.nixvim.plugins = {
+  # TODO
+  #   lsp = {
+  #     enable = true;
+  #     servers = {
+  #       nixd.enable = true;
+  #       qmlls.enable = true;
+  #       hls = {
+  #         enable = true;
+  #         installGhc = false;
+  #       };
+  #       clangd.enable = true;
+  #       rust_analyzer = {
+  #         enable = true;
+  #         installRustc = false;
+  #         installCargo = false;
+  #       };
+  #     };
+  #   };
+  # };
 }
