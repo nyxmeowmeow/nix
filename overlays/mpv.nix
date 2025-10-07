4069033433e210a3d9ff8161991e9ae4025699ad
nyxmeowmeow <rraaiin@proton.me>
2025-10-07 12:25:16 +1000

    07/10 12:25:16

---
+++ overlays/mpv.nix
@@ -0,0 +1,17 @@
+self: super: {
+  mpv = super.mpv.overrideAttrs (old: {
+    mesonBuildFlags = [
+      "-Dc_args=-O3 -march=native -pipe"
+    ];
+
+      waylandSupport = true;
+      x11Support = false;
+      sixelSupport = false;
+      alsaSupport = false;
+      bluraySupport = false;
+      pipewireSupport = true;
+      alsaSupport = false;
+      pulseSupport = false;
+      jackaudioSupport = false;
+  });
+}
