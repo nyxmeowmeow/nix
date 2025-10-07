b5da2834b6d96517ad0c0957b3b8e142b476cb2b
nyxmeowmeow <rraaiin@proton.me>
2025-10-07 12:50:23 +1000

    07/10 12:50:23

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,6 @@
 [
   (import ./foot.nix)
+  (import ./mpv.nix)
   (import ./nvim.nix)
   (import ./nushell.nix)
   (import ./yazi.nix)
--- overlays/mpv.nix
+++ overlays/mpv.nix
@@ -4,14 +4,13 @@ self: super: {
       "-Dc_args=-O3 -march=native -pipe"
     ];
 
-      waylandSupport = true;
-      x11Support = false;
-      sixelSupport = false;
-      alsaSupport = false;
-      bluraySupport = false;
-      pipewireSupport = true;
-      alsaSupport = false;
-      pulseSupport = false;
-      jackaudioSupport = false;
+    waylandSupport = true;
+    x11Support = false;
+    sixelSupport = false;
+    bluraySupport = false;
+    pipewireSupport = true;
+    alsaSupport = false;
+    pulseSupport = false;
+    jackaudioSupport = false;
   });
 }
