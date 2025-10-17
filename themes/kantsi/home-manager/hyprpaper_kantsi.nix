73c116d4c83e820fcba3c2468fed4694cc0c512f
nyxmeowmeow <rraaiin@proton.me>
2025-10-17 18:35:24 +1000

    17/10 18:35:24

--- themes/kantsi/home-manager/hyprpaper_kantsi.nix
+++ themes/kantsi/home-manager/hyprpaper_kantsi.nix
@@ -9,15 +9,12 @@
         splash_offset = 2.0;
 
         preload = [
-          # "${flake_dir}/stuff/wallpapers/topo_1.jpg" 
-          "${flake_dir}/stuff/wallpapers/meow.gif"
+          "${flake_dir}/stuff/wallpapers/topo_1.jpg" 
         ];
 
         wallpaper = [
-          # "DP-1, ${flake_dir}/stuff/wallpapers/topo_1.jpg"
-          # "DP-3, ${flake_dir}/stuff/wallpapers/topo_1.jpg"
-          "DP-1, ${flake_dir}/stuff/wallpapers/meow.gif"
-          "DP-3, ${flake_dir}/stuff/wallpapers/meow.gif"
+          "DP-1, ${flake_dir}/stuff/wallpapers/topo_1.jpg"
+          "DP-3, ${flake_dir}/stuff/wallpapers/topo_1.jpg"
         ];
       };
     };
