6ba6935817fece5d062c9c6073ca6fbb37655077
nyxmeowmeow <rraaiin@proton.me>
2025-10-17 18:27:08 +1000

    17/10 18:27:08

--- themes/kantsi/home-manager/hyprpaper_kantsi.nix
+++ themes/kantsi/home-manager/hyprpaper_kantsi.nix
@@ -9,12 +9,15 @@
         splash_offset = 2.0;
 
         preload = [
-          "${flake_dir}/stuff/wallpapers/topo_1.jpg" 
+          # "${flake_dir}/stuff/wallpapers/topo_1.jpg" 
+          "${flake_dir}/stuff/wallpapers/meow.gif"
         ];
 
         wallpaper = [
-          "DP-1, ${flake_dir}/stuff/wallpapers/topo_1.jpg"
-          "DP-3, ${flake_dir}/stuff/wallpapers/topo_1.jpg"
+          # "DP-1, ${flake_dir}/stuff/wallpapers/topo_1.jpg"
+          # "DP-3, ${flake_dir}/stuff/wallpapers/topo_1.jpg"
+          "DP-1, ${flake_dir}/stuff/wallpapers/meow.gif"
+          "DP-3, ${flake_dir}/stuff/wallpapers/meow.gif"
         ];
       };
     };
