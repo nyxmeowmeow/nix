81c643d363248da747320c47fbfb04cdb13b7561
nyxmeowmeow <rraaiin@proton.me>
2025-10-23 02:17:06 +1000

    23/10 02:17:06

--- modules/nixos/slippi.nix
+++ modules/nixos/slippi.nix
@@ -1,10 +1,12 @@
-{ inputs, username, ... }: {
+{ inputs, username, config, ... }: {
           home-manager = {
             users.${username} = {
               imports = [
                 inputs.slippi.homeManagerModules.default
                 {
                   slippi-launcher.isoPath = "/home/${username}/melee/meow.iso";
+                  slippi-launcher.rootSlpPath = "/home/${username}/.local/share/Slippi";
+                  slippi-launcher.spectateSlpPath = "/home/${username}/.local/share/Slippi/Spectate";
                   slippi-launcher.launchMeleeOnPlay = false;
                   slippi-launcher.enableJukebox = false;
                   slippi-launcher.useMonthlySubfolders = true;
