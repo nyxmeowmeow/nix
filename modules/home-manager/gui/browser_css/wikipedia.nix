40ccbf9ae8214b9a7f24f4b0ee3d7fadfc1fbb0a
nyxmeowmeow <rraaiin@proton.me>
2025-10-25 08:49:01 +1000

    25/10 08:49:01

--- modules/home-manager/gui/browser_css/wikipedia.nix
+++ modules/home-manager/gui/browser_css/wikipedia.nix
@@ -3,9 +3,31 @@ let
   theme_trimmed = lib.strings.removeSuffix "_zen" theme;
   col = import ../../../../themes/${theme_trimmed}/colors.nix;
 in {
-  home.file.".config/usercontent/github.css".text = /* css */ ''
-@-moz-document domain("github.com") {
+  home.file.".config/usercontent/wikipedia.css".text = /* css */ ''
+@-moz-document domain("wikipedia.com") {
+root: {
+  --font: ${config.stylix.fonts.monospace.name} !important;
+  --bg-color: transparent !important;
+  --background-color-base: transparent !important;
+}
 
+body {
+  background: transparent !important;
+  background-color: transparent !important;
+  font-family: ${config.stylix.fonts.monospace.name} !important;
+
+}
+
+@media screen {
+  html.skin-theme-clientpref-night {
+    color-scheme: dark;
+       --background-color-base: transparent !important;
+    --color-base: ${col.fg} !important;
+    --color-base--hover:${col.fg} !important;
+    --color-emphasized:${col.fg} !important;
+    --color-inverted: transparent !important;
+    }
+    }
 
 
 
