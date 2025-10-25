9875dc541046446fa6dfad783e8cb8f5517fc623
nyxmeowmeow <rraaiin@proton.me>
2025-10-25 15:41:28 +1000

    25/10 15:41:28

--- themes/kantsi/nixos/fzf_kantsi.nix
+++ themes/kantsi/nixos/fzf_kantsi.nix
@@ -5,7 +5,7 @@ in {
   config = lib.mkIf (config.theme == "kantsi") {
 
     environment.variables = {
-      # FZF_DEFAULT_OPTS = "--color=fg:${col.fg},hl:${col.blue},fg+:#000000,bg+:${col.blue},hl+:#8bd5ca,info:#7f8c8d,prompt:${col.blue},spinner:-1,pointer:${col.blue},gutter:-1,info:#939ab7,border:-1 --border=none --info=hidden --header='' --prompt='󰘧 ' --no-bold -i --pointer=''";
+      FZF_DEFAULT_OPTS = "--color=fg:${col.fg},hl:${col.blue},fg+:#000000,bg+:${col.blue},hl+:#8bd5ca,info:#7f8c8d,prompt:${col.blue},spinner:-1,pointer:${col.blue},gutter:-1,info:#939ab7,border:-1 --border=none --info=hidden --header='' --prompt='󰘧 ' --no-bold -i --pointer=''";
     };
   };
 }
