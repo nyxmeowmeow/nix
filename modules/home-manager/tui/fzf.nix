03e2021c22a66809d61195ac4b6b099ec6bde6f3
nyxmeowmeow <rraaiin@proton.me>
2025-10-25 15:43:08 +1000

    25/10 15:43:08

--- modules/home-manager/tui/fzf.nix
+++ modules/home-manager/tui/fzf.nix
@@ -1,35 +1,35 @@
 {
-  programs.fzf = {
-    enable = true;
-    enableBashIntegration = true;
-
-    #   # TODO move this to themes/*
-    colors = {
-      fg = "#A4A0E8";
-      hl = "#DBBFEF";
-      "fg+" = "#A4A0E8";
-      "bg+" = "#5A3D6E";
-      "hl+" = "#DBBFEF";
-      info = "#886C9C";
-      prompt = "#DBBFEF";
-      spinner = "-1";
-      pointer = "-1";
-      gutter = "-1";
-      border = "-1";
-    };
-    defaultCommand = "";
-    defaultOptions = [
-      "--margin=1"
-      # "--layout=reverse"
-      "--border=none"
-      "--info='hidden'"
-      "--header=''"
-      "--prompt='󰘧 '"
-      "-i"
-      "--no-bold"
-      # "--bind='enter:execute(nvim {})'"
-      # "--preview='bat --style=numbers --color=always --line-range :500 {}'"
-    ];
-  };
+  # programs.fzf = {
+  #   enable = true;
+  #   enableBashIntegration = true;
+  #
+  #   #   # TODO move this to themes/*
+  #   colors = {
+  #     fg = "#A4A0E8";
+  #     hl = "#DBBFEF";
+  #     "fg+" = "#A4A0E8";
+  #     "bg+" = "#5A3D6E";
+  #     "hl+" = "#DBBFEF";
+  #     info = "#886C9C";
+  #     prompt = "#DBBFEF";
+  #     spinner = "-1";
+  #     pointer = "-1";
+  #     gutter = "-1";
+  #     border = "-1";
+  #   };
+  #   defaultCommand = "";
+  #   defaultOptions = [
+  #     "--margin=1"
+  #     # "--layout=reverse"
+  #     "--border=none"
+  #     "--info='hidden'"
+  #     "--header=''"
+  #     "--prompt='󰘧 '"
+  #     "-i"
+  #     "--no-bold"
+  #     # "--bind='enter:execute(nvim {})'"
+  #     # "--preview='bat --style=numbers --color=always --line-range :500 {}'"
+  #   ];
+  # };
 }
 
