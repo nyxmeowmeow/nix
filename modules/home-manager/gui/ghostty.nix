17645f9fc325c56343c4c570445168a8396a0362
nyxmeowmeow <rraaiin@proton.me>
2025-10-29 06:18:48 +1000

    29/10 06:18:48

--- modules/home-manager/gui/ghostty.nix
+++ modules/home-manager/gui/ghostty.nix
@@ -2,4 +2,29 @@
   programs.ghostty = {
     enable = true;
   };
+
+systemd.user.services."app-com.mitchellh.ghostty" = {
+    Unit = {
+      Description = "Ghostty";
+      #After = [ "xdg-desktop-autostart.target" ];
+      After = [
+        "graphical-session.target"
+        "dbus.socket"
+      ];
+      Requires = [ "dbus.socket" ];
+    };
+
+    Service = {
+      #Type = "dbus";
+      Type = "notify-reload";
+      BusName = "com.mitchellh.ghostty";
+      ReloadSignal = "SIGUSR2";
+      ExecStart = "ghostty --gtk-single-instance=true --initial-window=false --quit-after-last-window-closed=false";
+    };
+
+    Install = {
+      #WantedBy = [ "xdg-desktop-autostart.target" ];
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
 }
