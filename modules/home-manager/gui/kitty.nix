50e99314a956b0b6092379f0bde2210a1c36ee24
nyxmeowmeow <rraaiin@proton.me>
2025-10-31 05:37:02 +1000

    31/10 05:37:02

--- modules/home-manager/gui/kitty.nix
+++ modules/home-manager/gui/kitty.nix
@@ -4,7 +4,25 @@
 }: {
   programs.kitty = {
     enable = true;
-    # font.name = config.stylix.fonts.monospace.name;
-    # font.size = 22;
+    font.name = config.stylix.fonts.monospace.name;
+    font.size = 22;
+    settings = {
+      enable_audio_bell = false;
+      disable_ligatures = true;
+      box_drawing_scale = "2 2 2 2";
+
+      cursor_shape_unfocused = "unchanged";
+
+      cursor_trail = "1";
+
+      sync_to_monitor = true;
+
+      remember_window_size = false;
+      remember_window_position = false;
+
+      window_padding_width = "1 2";
+      
+      wayland_enable_ime = false;
+    };
   };
 }
