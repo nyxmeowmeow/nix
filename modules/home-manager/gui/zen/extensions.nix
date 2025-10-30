312577ebf5561b60ba00ef68bab4567c16afd5f2
nyxmeowmeow <rraaiin@proton.me>
2025-10-31 03:48:57 +1000

    31/10 03:48:57 move separate shared zen extensions

--- modules/home-manager/gui/zen/extensions.nix
+++ modules/home-manager/gui/zen/extensions.nix
@@ -5,51 +5,39 @@
   ...
 }:
 let
-shared_extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-ublock-origin
-darkreader
-bitwarden
-];
-
-  meow_extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-    #bypass-paywalls-clean
-    indie-wiki-buddy
+  shared_extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+    ublock-origin
+    darkreader
+    bitwarden
+    userchrome-toggle
+    user-agent-string-switcher
     stylus
     canvasblocker
-    fastforwardteam
     tridactyl
     clearurls
+    fastforwardteam
     shinigami-eyes
+    #bypass-paywalls-clean
     pronoundb
-    # containerise
     don-t-fuck-with-paste
     google-container
+    # containerise
+  ];
+
+  meow_extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+    indie-wiki-buddy
     lovely-forks
     refined-github
     github-issue-link-status
     github-file-icons
-    user-agent-string-switcher
-    userchrome-toggle
   ] ++ shared_extensions;
 
   media_extensions = with pkgs.nur.repos.rycee.firefox-addons; [
     sponsorblock
     return-youtube-dislikes
-    #bypass-paywalls-clean
-    stylus
-    canvasblocker
-    fastforwardteam
-    tridactyl
-    clearurls
     # "7tv"
     betterttv
-    shinigami-eyes
-    pronoundb
     blocktube
-    don-t-fuck-with-paste
-    google-container
-    user-agent-string-switcher
-    userchrome-toggle
     # TODO alternate player for twitch
   ] ++ shared_extensions;
 
