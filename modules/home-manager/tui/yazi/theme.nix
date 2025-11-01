462b6c89a3d2f4f46fcfe84508c4f044021c6c77
nyxmeowmeow <rraaiin@proton.me>
2025-11-01 18:56:42 +1000

    01/11 18:56:42 yazi tabs theme

--- modules/home-manager/tui/yazi/theme.nix
+++ modules/home-manager/tui/yazi/theme.nix
@@ -1,9 +1,27 @@
-{
+{ config, lib, theme, ... }:
+let
+  theme_trimmed = lib.strings.removeSuffix "_zen" theme;
+  col = import ../../../../themes/${theme_trimmed}/colors.nix;
+in {
   programs.yazi.theme = {
     mgr = {
       border_symbol = " ";
       hovered.reversed = true;
       preview_hovered.underline = true;
+    };
+    tabs = {
+      active = {
+        bg = col.accent;
+        fg = col.bg;
+      };
+      inactive = {
+        bg = col.bg;
+        fg = col.accent;
+      };
+      sep_inner = {
+        open = "";
+        close = "";
+      };
     };
   };
 }
