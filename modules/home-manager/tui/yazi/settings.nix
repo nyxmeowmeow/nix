5255b81c87c9660ba55f9c1ea89b8594104cea78
nyxmeowmeow <rraaiin@proton.me>
2025-11-01 19:52:22 +1000

    01/11 19:52:22

--- modules/home-manager/tui/yazi/settings.nix
+++ modules/home-manager/tui/yazi/settings.nix
@@ -286,200 +286,200 @@
             ];
 
             spotters = [
-            {
-              name = "*/";
-              run = "folder";
-            }
-
-# Code
-            {
-              mime = "text/*";
-              run = "code";
-            }
-
-            {
-              mime = "application/{mbox,javascript,wine-extension-ini}";
-              run = "code";
-            }
+#             {
+#               name = "*/";
+#               run = "folder";
+#             }
+#
+# # Code
+#             {
+#               mime = "text/*";
+#               run = "code";
+#             }
+#
+#             {
+#               mime = "application/{mbox,javascript,wine-extension-ini}";
+#               run = "code";
+#             }
 
 # Image
             {
               name = "*.dds";
-              run = ''piper -- kitten icat $1'';
+              run = ''piper -- kitten icat "$1"'';
             }
 
-            {
-              mime = "image/{avif,hei?,jxl,svg+xml}";
-              run = "magick";
-            }
-
-            {
-              mime = "image/*";
-              run = "image";
-            }
-
-# Video
-            {
-              mime = "video/*";
-              run = "video";
-            }
-
-# Fallback
-            {
-              name = "*";
-              run = "file";
-            }
+#             {
+#               mime = "image/{avif,hei?,jxl,svg+xml}";
+#               run = "magick";
+#             }
+#
+#             {
+#               mime = "image/*";
+#               run = "image";
+#             }
+#
+# # Video
+#             {
+#               mime = "video/*";
+#               run = "video";
+#             }
+#
+# # Fallback
+#             {
+#               name = "*";
+#               run = "file";
+#             }
             ];
 
             preloaders = [
 # Image
             {
               name = "*.dds";
-              run = ''piper -- kitten icat $1'';
+              run = ''piper -- kitten icat "$1"'';
             }
 
-            {
-              mime = "image/{avif,hei?,jxl,svg+xml}";
-              run = "magick";
-            }
-
-            {
-              mime = "image/*";
-              run = "image";
-            }
-
-# Video
-            {
-              mime = "video/*";
-              run = "video";
-            }
-
-# PDF
-            {
-              mime = "application/pdf";
-              run = "pdf";
-            }
-
-# Font
-            {
-              mime = "font/*";
-              run = "font";
-            }
-
-            {
-              mime = "application/ms-opentype";
-              run = "font";
-            }
+#             {
+#               mime = "image/{avif,hei?,jxl,svg+xml}";
+#               run = "magick";
+#             }
+#
+#             {
+#               mime = "image/*";
+#               run = "image";
+#             }
+#
+# # Video
+#             {
+#               mime = "video/*";
+#               run = "video";
+#             }
+#
+# # PDF
+#             {
+#               mime = "application/pdf";
+#               run = "pdf";
+#             }
+#
+# # Font
+#             {
+#               mime = "font/*";
+#               run = "font";
+#             }
+#
+#             {
+#               mime = "application/ms-opentype";
+#               run = "font";
+#             }
             ];
 
             previewers = [
-            {
-              name = "*/";
-              run = "folder";
-              sync = true;
-            }
-
-# Code
-            {
-              mime = "text/*";
-              run = "code";
-            }
-
-            {
-              mime = "application/{mbox,javascript,wine-extension-ini}";
-              run = "code";
-            }
-
-# JSON
-            {
-              mime = "application/{json,ndjson}";
-              run = "json";
-            }
+#             {
+#               name = "*/";
+#               run = "folder";
+#               sync = true;
+#             }
+#
+# # Code
+#             {
+#               mime = "text/*";
+#               run = "code";
+#             }
+#
+#             {
+#               mime = "application/{mbox,javascript,wine-extension-ini}";
+#               run = "code";
+#             }
+#
+# # JSON
+#             {
+#               mime = "application/{json,ndjson}";
+#               run = "json";
+#             }
 
 # Image
             {
               name = "*.dds";
-              run = ''piper -- kitten icat $1'';
+              run = ''piper -- kitten icat "$1"'';
             }
 
-            {
-              mime = "image/{avif,hei?,jxl,svg+xml}";
-              run = "magick";
-            }
-
-            {
-              mime = "image/*";
-              run = "image";
-            }
-
-# Video
-            {
-              mime = "video/*";
-              run = "video";
-            }
-
-# PDF
-            {
-              mime = "application/pdf";
-              run = "pdf";
-            }
-
-# Archive
-            {
-              mime = "application/{zip,rar,7z*,tar,gzip,xz,zstd,bzip*,lzma,compress,archive,cpio,arj,xar,ms-cab*}";
-              run = "archive";
-            }
-
-            {
-              mime = "application/{debian*-package,redhat-package-manager,rpm,android.package-archive}";
-              run = "archive";
-            }
-
-# { name = "*.{AppImage,appimage}", run = "archive" },
-
-            {
-              name = "*.{AppImage,appimage}";
-              run = "appimaged";
-            }
-
-# Virtual Disk / Disk Image
-            {
-              mime = "application/{iso9660-image,qemu-disk,ms-wim,apple-diskimage}";
-              run = "archive";
-            }
-
-            {
-              mime = "application/virtualbox-{vhd,vhdx}";
-              run = "archive";
-            }
-
-            {
-              name = "*.{img,fat,ext,ext2,ext3,ext4,squashfs,ntfs,hfs,hfsx}";
-              run = "archive";
-            }
-
-# Font
-            {
-              mime = "font/*";
-              run = "font";
-            }
-
-            {
-              mime = "application/ms-opentype";
-              run = "font";
-            }
-
-# Empty file
-            {
-              mime = "inode/empty";
-              run = "empty";
-            }
-
-# Fallback
-            {
-              name = "*";
-              run = "file";
-            }
+#             {
+#               mime = "image/{avif,hei?,jxl,svg+xml}";
+#               run = "magick";
+#             }
+#
+#             {
+#               mime = "image/*";
+#               run = "image";
+#             }
+#
+# # Video
+#             {
+#               mime = "video/*";
+#               run = "video";
+#             }
+#
+# # PDF
+#             {
+#               mime = "application/pdf";
+#               run = "pdf";
+#             }
+#
+# # Archive
+#             {
+#               mime = "application/{zip,rar,7z*,tar,gzip,xz,zstd,bzip*,lzma,compress,archive,cpio,arj,xar,ms-cab*}";
+#               run = "archive";
+#             }
+#
+#             {
+#               mime = "application/{debian*-package,redhat-package-manager,rpm,android.package-archive}";
+#               run = "archive";
+#             }
+#
+# # { name = "*.{AppImage,appimage}", run = "archive" },
+#
+#             {
+#               name = "*.{AppImage,appimage}";
+#               run = "appimaged";
+#             }
+#
+# # Virtual Disk / Disk Image
+#             {
+#               mime = "application/{iso9660-image,qemu-disk,ms-wim,apple-diskimage}";
+#               run = "archive";
+#             }
+#
+#             {
+#               mime = "application/virtualbox-{vhd,vhdx}";
+#               run = "archive";
+#             }
+#
+#             {
+#               name = "*.{img,fat,ext,ext2,ext3,ext4,squashfs,ntfs,hfs,hfsx}";
+#               run = "archive";
+#             }
+#
+# # Font
+#             {
+#               mime = "font/*";
+#               run = "font";
+#             }
+#
+#             {
+#               mime = "application/ms-opentype";
+#               run = "font";
+#             }
+#
+# # Empty file
+#             {
+#               mime = "inode/empty";
+#               run = "empty";
+#             }
+#
+# # Fallback
+#             {
+#               name = "*";
+#               run = "file";
+#             }
             ];
           };
         };
