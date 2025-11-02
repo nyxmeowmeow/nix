70c73a30ee43aa9de9fd88ed30e0503fc9be0e64
nyxmeowmeow <rraaiin@proton.me>
2025-11-02 10:31:44 +1000

    02/11 10:31:44

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -8,7 +8,8 @@
       name = "melee";
       comment = "melee bash script";
       exec = "${flake_dir}/stuff/scripts/melee.sh";
-      icon = "${inputs.slippi}/usr/share/icons/hicolor/512x512/apps"; # FIXME
+      # icon = "${inputs.slippi}/usr/share/icons/hicolor/512x512/apps"; # FIXME
+      icon = "${inputs.slippi.packages."x86_64-linux".slippi-launcher}/usr/share/icons/hicolor/512x512/apps/slippi-launcher.png";
       terminal = true;
     };
 
