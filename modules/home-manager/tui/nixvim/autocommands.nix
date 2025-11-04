4ba48b551dca00ddf6b519d25c1004aea03bd169
nyxmeowmeow <rraaiin@proton.me>
2025-11-04 15:18:14 +1000

    04/11 15:18:14

--- modules/home-manager/tui/nixvim/autocommands.nix
+++ modules/home-manager/tui/nixvim/autocommands.nix
@@ -8,5 +8,13 @@
     pattern = "*.md";
     command = "setlocal nonumber norelativenumber signcolumn=no";
   }
+  {
+    event = [
+      "BufEnter"
+      "BufWinEnter"
+    ];
+    pattern = "*";
+    command = "loadview"; # load folds
+  }
   ];
 }
