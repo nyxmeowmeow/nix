f975686a5b0cb57bd4706f16365a196883a5c7b4
nyxmeowmeow <rraaiin@proton.me>
2025-11-05 18:37:55 +1000

    05/11 18:37:55 gtk theme

--- themes/kanso/home-manager/gtk_kanso.nix
+++ themes/kanso/home-manager/gtk_kanso.nix
@@ -1,28 +1,42 @@
-{ lib, theme, pkgs, ... }: {
-  config = lib.mkIf (theme == "kanso_mist" || theme == "kanso_ink" || theme == "kanso_zen") {
+{
+  lib,
+  theme,
+  pkgs,
+  config,
+  ...
+}: {
+  config = lib.mkIf (lib.hasPrefix "kanso" theme) {
 
     gtk = {
       enable = true;
       theme = {
-        name = "catppuccin-macchiato-lavender-compact";
-        package = pkgs.catppuccin-gtk.override {
-          variant = "macchiato";
-          accents = [ "lavender" ]; # can specify multiple accents
-            size = "compact"; # compact, standard, large
-            # tweaks = [ "rimless" "black" ];
+        name = "fluent-compact";
+        package = pkgs.fluent-gtk-theme.override {
+          themeVariants = [ "purple" ];
+          colorVariants = [ "dark" ];
+          sizeVariants = [ "compact" ];
+          tweaks = [
+            # "solid"
+            "float"
+            # "round"
+            # "blur"
+            # "noborder"
+            # "square"
+          ];
         };
       };
 
-      iconTheme = {
-        # package = pkgs.gnome.adwaita-icon-theme;
-        name = "Tela-circle-dracula";
-      };
+      # iconTheme = {
+      #   package = pkgs.gnome.adwaita-icon-theme;
+      #   name = "Tela-circle-dracula";
+      # };
 
       font = {
-        name = "sn pro";
-        size = 11;
+        name = config.stylix.fonts.monospace.name;
+        size = 12;
       };
 
+      # emacs keybinds for text fields
       gtk3.extraConfig.gtk-key-theme-name = "Emacs";
       gtk4.extraConfig.gtk-key-theme-name = "Emacs";
     };
