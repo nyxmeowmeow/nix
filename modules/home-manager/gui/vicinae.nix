403135fecf57e857cca9d909adf276e80fc8eea5
nyxmeowmeow <rraaiin@proton.me>
2025-11-07 07:21:30 +1000

    07/11 07:21:30

--- modules/home-manager/gui/vicinae.nix
+++ modules/home-manager/gui/vicinae.nix
@@ -1,8 +1,42 @@
 {
   inputs,
   pkgs,
+  config,
     ...
 }: {
+
+    lib.vicinae.mkRayCastExtension = (
+      {
+        name,
+        sha256,
+        rev,
+      }:
+      let
+        src =
+          pkgs.fetchgit {
+            inherit rev sha256;
+            url = "https://github.com/raycast/extensions";
+            sparseCheckout = [
+              "/extensions/${name}"
+            ];
+          }
+          + "/extensions/${name}";
+      in
+      (pkgs.buildNpmPackage {
+        inherit name src;
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out
+          cp -r /build/.config/raycast/extensions/${name}/* $out/
+          runHook postInstall
+        '';
+        npmDeps = pkgs.importNpmLock { npmRoot = src; };
+        npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+      })
+    );
+
+
+
   imports = [
     inputs.vicinae.homeManagerModules.default
   ];
@@ -21,23 +55,8 @@
 #       rounding = 10;
 #     };
 # };
-# # Installing (vicinae) extensions declaratively
-
-
-# raycast://extensions/thomas/color-picker?source=webstore
 extensions = [
-#   (inputs.vicinae.mkVicinaeExtension.${pkgs.system} {
-#     inherit pkgs;
-#     name = "balatro-compendium";
-#     src = pkgs.fetchFromGitHub { # You can also specify different sources other than github
-#       owner = "raycast";
-#       repo = "extensions/tree/main/extensions/balatro-compendium";
-#       rev = "f4b1ca09af17e754f02437a3f6145f45327892bf"; # If the extension has no releases use the latest commit hash
-#       # You can get the sha256 by rebuilding once and then copying the output hash from the error message
-#       sha256 = "sha256-kbsdvIdye4/qQEEmIYh1K1vzlz+rcKJJOJo0Rv9if+s=";
-#     };
-# # If the extension is in a subdirectory you can add ` + "/subdir"` between the brace and the semicolon here
-#   })
+# https://github.com/nix-community/home-manager/commit/cc837038306d752355d21a4b4620ff589bcd1e71
   (inputs.vicinae.mkVicinaeExtension.${pkgs.system} {
     inherit pkgs;
     name = "case-converter";
@@ -47,6 +66,36 @@ extensions = [
       rev = "12fefbb2f852c38ac1b418c5955ed6ab070bafe3";
       sha256 = "sha256-6eDqc98qzg1y+O7pbfS69gEelTVPqi+7PK7X1a+S5qE=";
     } + "/case-converter";
+  })
+
+  # FIXME
+  # (inputs.vicinae.mkVicinaeExtension.${pkgs.system} {
+  #   inherit pkgs;
+  #   name = "unicode-symbols";
+  #   src = pkgs.fetchFromGitHub {
+  #     owner = "nyxmeowmeow";
+  #     repo = "vicinae-extensions";
+  #     rev = "da27028f4c22344cc6e4d4e05e1b6f13235b2f20";
+  #     sha256 = "sha256-J0uQtN0/FyuG6YB3FStUg1PTt1JPUxgpopHuPnrINhE=";
+  #   } + "/unicode-symbols";
+  # })
+
+  # (inputs.vicinae.mkRayCastExtension.${pkgs.system} {
+  #   inherit pkgs;
+  #   name = "balatro-compendium";
+  #   src = pkgs.fetchgit {
+  #           url = "https://github.com/raycast/extensions";
+  #           sparseCheckout = [
+  #             "/extensions/balatro-compendium"
+  #           ];
+  #         }
+  #         + "/extensions/balatro-compendium";
+  #
+  # })
+  (config.lib.vicinae.mkRayCastExtension {
+  name = "gif-search";
+  sha256 = "sha256-G7il8T1L+P/2mXWJsb68n4BCbVKcrrtK8GnBNxzt73Q=";
+  rev = "4d417c2dfd86a5b2bea202d4a7b48d8eb3dbaeb1";
   })
 ];
   };
