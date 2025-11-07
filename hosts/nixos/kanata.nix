e4dc8e92b15a5981ccae7cc0ba9c6fa9127d2a77
nyxmeowmeow <rraaiin@proton.me>
2025-11-08 09:07:08 +1000

    08/11 09:07:08

--- hosts/nixos/kanata.nix
+++ hosts/nixos/kanata.nix
@@ -220,6 +220,7 @@
 
 
 
+    ((key-history 0 1)) (macro .) break
 
     ;; &&
     ((key-history 7 1)) (macro S-7) break
@@ -265,8 +266,7 @@
     ((key-history ; 1)) (macro S-;) break
     ((key-history 7 1)) (macro S-7) break
     ((key-history 9 1)) (macro S-9) break
-    ;; ((key-history 0 1)) (macro S-0) break
-    ((key-history 0 1)) (macro .) break
+    ((key-history 0 1)) (macro S-0) break
 
     ((key-history e 1)) (macro u) break
 
