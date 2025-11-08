ea1391d9f0f123ed6e3c5c1610f58ac2910300cf
nyxmeowmeow <rraaiin@proton.me>
2025-11-08 13:07:39 +1000

    08/11 13:07:39

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -3,33 +3,33 @@
     enable = true;
     autoEnable = false; # manually specify which to enable
 
-      targets = {
-        gtk.enable = true;
-        qt.enable = true;
-      };
+      # targets = {
+      #   gtk.enable = true;
+      #   qt.enable = true;
+      # };
 
 
     image = ../../stuff/wallpapers/thing.png; # must be set for some reason
 
-      base16Scheme = {
-        scheme = "kanso";
-        base00 = "#ffffff";
-        base01 = "#e3e3e3";
-        base02 = "#b9b9b9";
-        base03 = "#ababab";
-        base04 = "#525252";
-        base05 = "#464646";
-        base06 = "#252525";
-        base07 = "#000000";
-        base08 = "#7c7c7c";
-        base09 = "#999999";
-        base0A = "#a0a0a0";
-        base0B = "#8e8e8e";
-        base0C = "#868686";
-        base0D = "#686868";
-        base0E = "#747474";
-        base0F = "#5e5e5e";
-      };
+      # base16Scheme = {
+      #   scheme = "kanso";
+      #   base00 = "#ffffff";
+      #   base01 = "#e3e3e3";
+      #   base02 = "#b9b9b9";
+      #   base03 = "#ababab";
+      #   base04 = "#525252";
+      #   base05 = "#464646";
+      #   base06 = "#252525";
+      #   base07 = "#000000";
+      #   base08 = "#7c7c7c";
+      #   base09 = "#999999";
+      #   base0A = "#a0a0a0";
+      #   base0B = "#8e8e8e";
+      #   base0C = "#868686";
+      #   base0D = "#686868";
+      #   base0E = "#747474";
+      #   base0F = "#5e5e5e";
+      # };
 
 
     cursor = {
