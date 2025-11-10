dbf43f30cc64674830af31d35b63347d9c5a7de6
nyxmeowmeow <rraaiin@proton.me>
2025-11-11 01:28:28 +1000

    11/11 01:28:28

--- modules/home-manager/tui/nixvim/utils/hardtime.nix
+++ modules/home-manager/tui/nixvim/utils/hardtime.nix
@@ -1,15 +1,15 @@
 {
-  programs.nixvim.plugins.hardtime = {
-    enable = true;
-    settings = {
-      cmd = "Neotest";
-      keys = [
-        {
-          __unkeyed-1 = "<leader>nt";
-          __unkeyed-3 = "<CMD>Neotest summary<CR>";
-          desc = "Summary toggle";
-        }
-      ];
-    };
-  };
+  # programs.nixvim.plugins.hardtime = {
+  #   enable = true;
+  #   settings = {
+  #     cmd = "Neotest";
+  #     keys = [
+  #       {
+  #         __unkeyed-1 = "<leader>nt";
+  #         __unkeyed-3 = "<CMD>Neotest summary<CR>";
+  #         desc = "Summary toggle";
+  #       }
+  #     ];
+  #   };
+  # };
 }
