c75ebc5050d9f50d92c38171619558bfd3894b55
nyxmeowmeow <rraaiin@proton.me>
2025-11-13 04:32:47 +1000

    13/11 04:32:47

--- modules/home-manager/gui/browser_css/git/codeberg.nix
+++ modules/home-manager/gui/browser_css/git/codeberg.nix
@@ -1,4 +1,9 @@
-{ config, lib, theme, ... }:
+{
+  config,
+  lib,
+  theme,
+  ...
+}:
 let
   theme_trimmed = lib.strings.removeSuffix "_zen" theme;
   col = import ../../../../../themes/${theme_trimmed}/colors.nix;
@@ -6,29 +11,33 @@ in {
   home.file.".config/usercontent/codeberg.css".text = /* css */ ''
 @-moz-document domain("codeberg.org") {
 * {
-    font-family: ${config.stylix.fonts.monospace.name} !important;
-    border-color: transparent !important;
+  font-family: ${config.stylix.fonts.monospace.name} !important;
+  border-color: transparent !important;
 }
 
 :root {
-    --color-text: ${col.fg} !important;
-    --color-body: transparent !important;
-    --color-secondary-nav-bg: transparent !important;
-    --color-text-light-2: ${col.grey} !important;
-    --color-button: ${col.button} !important;
-    --color-box-body: transparent !important;
-    --color-input-background: ${col.button} !important;
-    --color-secondary-light-1: transparent !important;
-    --color-box-header: transparent !important;
-    --color-hover: ${col.button_hover} !important;
+  --color-text: ${col.fg} !important;
+  --color-body: transparent !important;
+  --color-secondary-nav-bg: transparent !important;
+  --color-text-light-2: ${col.grey} !important;
+  --color-button: ${col.button} !important;
+  --color-box-body: transparent !important;
+  --color-input-background: ${col.button} !important;
+  --color-secondary-light-1: transparent !important;
+  --color-box-header: transparent !important;
+  --color-hover: ${col.button_hover} !important;
+  --color-secondary-dark-1: transparent !important;
+  --color-primary-hover: ${col.accent} !important;
+  --color-primary: ${col.accent} !important;
 }
 
-#navbar {
-    background: transparent !important;
+#navbar,
+.tw-bg-box-header {
+  background: transparent !important;
 }
 
 #navbar .navbar-left {
-    display: none !important;
+  display: none !important;
 }
 #navbar .navbar-right {
   position: absolute !important;
@@ -42,14 +51,7 @@ in {
   border-bottom-right-radius: 99px !important;
 }
 
-.tw-bg-box-header {
-  background-color: ${col.grey} !important;
-}
-
-.tw-border-secondary {
-  border-color: transparent !important;
-}
-
+.tw-border-secondary,
 .ui.table {
   border: none;
 }
--- modules/home-manager/gui/browser_css/git/sourcehut.nix
+++ modules/home-manager/gui/browser_css/git/sourcehut.nix
@@ -1,4 +1,9 @@
-{ config, lib, theme, ... }:
+{
+  config,
+  lib,
+  theme,
+  ...
+}:
 let
   theme_trimmed = lib.strings.removeSuffix "_zen" theme;
   col = import ../../../../../themes/${theme_trimmed}/colors.nix;
