9fcaa9744b7dba846c56e2046c749289e7c106c0
nyxmeowmeow <rraaiin@proton.me>
2025-11-14 01:56:20 +1000

    14/11 01:56:20

--- modules/nixos/env.nix
+++ modules/nixos/env.nix
@@ -45,41 +45,4 @@ in {
     VIDEOS = "${h}/vid";
 
   };
-
-  environment.variables = {
-    ELECTRON_OZONE_PLATFORM_HINT = "auto";
-
-    XDG_CONFIG_HOME = "${h}/.config";
-    XDG_DATA_HOME = "${h}/.local/share";
-    XDG_CACHE_HOME = "${h}/.cache";
-
-    CALCHISTFILE = "${cache}/calc_history";
-    CARGO_HOME = "${data}/cargo";
-    DOCKER_CONFIG = "${cfg}/docker";
-    GNUPGHOME = "${data}/gnupg";
-    GTK2_RC_FILES = "${cfg}/gtk-2.0/gtkrc";
-    GTK_RC_FILES = "${cfg}/gtk-1.0/gtkrc";
-    KDEHOME = "${cfg}/kde";
-    NODE_REPL_HISTORY = "${data}/node_repl_history";
-    NPM_CONFIG_USERCONFIG = "${cfg}/npm/npmrc";
-    PYTHONHISTORY = "${data}/python_history";
-    RUSTUP_HOME = "${data}/rustup";
-    WGETRC = "${cfg}/wgetrc";
-    WINEPREFIX = "${data}/wineprefixes/default";
-    XINITRC = "${cfg}/X11/xinitrc";
-    ZDOTDIR = "${cfg}/zsh";
-    ZSH_DATA_PATH = "${data}/zsh";
-    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${cfg}/java";
-    GOPATH = "${data}/go";
-    GOMODCACHE = "${data}/go/mod";
-
-
-    DESKTOP = "/tmp/desktop";
-    DOCUMENTS = "${h}/doc";
-    DOWNLOAD = "${h}/dl";
-    MUSIC = "${h}/music";
-    PICTURES = "${h}/pic";
-    VIDEOS = "${h}/vid";
-
-  };
 }
