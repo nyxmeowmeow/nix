571737d63475b46296143a50a1092b74ce47a83e
nyxmeowmeow <rraaiin@proton.me>
2025-11-13 21:50:13 +1000

    13/11 21:50:13

--- modules/home-manager/tui/yazi/settings.nix
+++ modules/home-manager/tui/yazi/settings.nix
@@ -1,109 +1,109 @@
 {
   programs.yazi = {
     settings = {
-      # mgr = {
-      #   ratio          = [ 0 3 4 ];
-      #   sort_by        = "natural";
-      #   sort_sensitive = false;
-      #   sort_reverse 	 = false;
-      #   sort_dir_first = true;
-      #   sort_translit  = false;
-      #   linemode       = "none";
-      #   show_hidden    = true;
-      #   show_symlink   = true;
-      #   scrolloff      = 10;
-      #   mouse_events   = [];
-      #   title_format   = "Yazi: {cwd}";
-      # };
-      #
-      # preview = {
-      #   tab_size        = 2;
-      #   max_width       = 2560;
-      #   max_height      = 1440;
-      #   cache_dir       = "";
-      #   image_delay     = 0;
-      #   image_filter    = "triangle";
-      #   image_quality   = 85;
-      #   sixel_fraction  = 1;
-      #   ueberzug_scale  = 1;
-      #   ueberzug_offset = [ 0 0 0 0 ];
-      # };
-      #
+      mgr = {
+        ratio          = [ 0 3 4 ];
+        sort_by        = "natural";
+        sort_sensitive = false;
+        sort_reverse 	 = false;
+        sort_dir_first = true;
+        sort_translit  = false;
+        linemode       = "none";
+        show_hidden    = true;
+        show_symlink   = true;
+        scrolloff      = 10;
+        mouse_events   = [];
+        title_format   = "Yazi: {cwd}";
+      };
 
-#       input = {
-#         cursor_blink = false;
-#
-# # cd
-#         cd_title  = "";
-#         cd_origin = "center";
-#         cd_offset = [ 0 0 50 3 ];
-#
-# # create
-#         create_title  = [ "" "" ];
-#         create_origin = "center";
-#         create_offset = [ 0 0 50 3 ];
-#
-# # rename
-#         rename_title  = "";
-#         rename_origin = "hovered";
-#         rename_offset = [ 0 1 50 3 ];
-#
-# # filter
-#         filter_title  = "";
-#         filter_origin = "center";
-#         filter_offset = [ 0 0 50 3 ];
-#
-# # find
-#         find_title  = [ "" "" ];
-#         find_origin = "center";
-#         find_offset = [ 0 0 50 3 ];
-#
-# # search
-#         search_title  = " {n} ";
-#         search_origin = "center";
-#         search_offset = [ 0 0 50 3 ];
-#
-# # shell
-#         shell_title  = [ "" "" ];
-#         shell_origin = "center";
-#         shell_offset = [ 0 0 50 3 ];
-#
-#         confirm = {
-# # trash
-#           trash_title 	= " Trash {n} selected file{s}? ";
-#           trash_origin	= "center";
-#           trash_offset	= [ 0 0 70 20 ];
-#
-# # delete
-#           delete_title 	= " PERMANENTLY delete {n} selected file{s}? ";
-#           delete_origin	= "center";
-#           delete_offset	= [ 0 0 70 20 ];
-#
-# # overwrite
-#           overwrite_title   = " Overwrite file? ";
-#           overwrite_content = "Will overwrite the following file:";
-#           overwrite_origin  = "center";
-#           overwrite_offset  = [ 0 0 50 15 ];
-#
-# # quit
-#           quit_title   = " Quit? ";
-#           quit_content = "The following tasks are still running, are you sure you want to quit?";
-#           quit_origin  = "center";
-#           quit_offset  = [ 0 0 50 15 ];
-#         };
-#
-#         pick = {
-#           open_title  = "";
-#           open_origin = "hovered";
-#           open_offset = [ 0 1 50 7 ];
-#         };
-#
-#         which = {
-#           sort_by      	 = "none";
-#           sort_sensitive = false;
-#           sort_reverse 	 = false;
-#           sort_translit  = false;
-#         };
+      preview = {
+        tab_size        = 2;
+        max_width       = 2560;
+        max_height      = 1440;
+        cache_dir       = "";
+        image_delay     = 0;
+        image_filter    = "triangle";
+        image_quality   = 85;
+        sixel_fraction  = 1;
+        ueberzug_scale  = 1;
+        ueberzug_offset = [ 0 0 0 0 ];
+      };
+
+
+      input = {
+        cursor_blink = false;
+
+# cd
+        cd_title  = "";
+        cd_origin = "center";
+        cd_offset = [ 0 0 50 3 ];
+
+# create
+        create_title  = [ "" "" ];
+        create_origin = "center";
+        create_offset = [ 0 0 50 3 ];
+
+# rename
+        rename_title  = "";
+        rename_origin = "hovered";
+        rename_offset = [ 0 1 50 3 ];
+
+# filter
+        filter_title  = "";
+        filter_origin = "center";
+        filter_offset = [ 0 0 50 3 ];
+
+# find
+        find_title  = [ "" "" ];
+        find_origin = "center";
+        find_offset = [ 0 0 50 3 ];
+
+# search
+        search_title  = " {n} ";
+        search_origin = "center";
+        search_offset = [ 0 0 50 3 ];
+
+# shell
+        shell_title  = [ "" "" ];
+        shell_origin = "center";
+        shell_offset = [ 0 0 50 3 ];
+
+        confirm = {
+# trash
+          trash_title 	= " Trash {n} selected file{s}? ";
+          trash_origin	= "center";
+          trash_offset	= [ 0 0 70 20 ];
+
+# delete
+          delete_title 	= " PERMANENTLY delete {n} selected file{s}? ";
+          delete_origin	= "center";
+          delete_offset	= [ 0 0 70 20 ];
+
+# overwrite
+          overwrite_title   = " Overwrite file? ";
+          overwrite_content = "Will overwrite the following file:";
+          overwrite_origin  = "center";
+          overwrite_offset  = [ 0 0 50 15 ];
+
+# quit
+          quit_title   = " Quit? ";
+          quit_content = "The following tasks are still running, are you sure you want to quit?";
+          quit_origin  = "center";
+          quit_offset  = [ 0 0 50 15 ];
+        };
+
+        pick = {
+          open_title  = "";
+          open_origin = "hovered";
+          open_offset = [ 0 1 50 7 ];
+        };
+
+        which = {
+          sort_by      	 = "none";
+          sort_sensitive = false;
+          sort_reverse 	 = false;
+          sort_translit  = false;
+        };
 
 
 
@@ -502,7 +502,7 @@
 #             }
 #             ];
 #           };
-#         };
+        };
       };
     };
   };
