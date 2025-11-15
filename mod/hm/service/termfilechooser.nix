c93ee081f9cc5a3df4cbfc2cf18e2f89436ab91d
nyxmeowmeow <rraaiin@proton.me>
2025-11-16 04:29:28 +1000

    16/11 04:29:28

--- mod/hm/service/termfilechooser.nix
+++ mod/hm/service/termfilechooser.nix
@@ -13,4 +13,16 @@
       RestartSec = 3;
     };
   };
+
+  home.file.".config/xdg-desktop-portal-termfilechooser/config".text = ''
+[filechooser]
+cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+default_dir=$HOME
+env=TERMCMD=kitty --app-id="kitty.yazi.filechooser"
+  '';
+
+  home.file.".config/xdg-desktop-portal/portals.conf".text = ''
+[preferred]
+org.freedesktop.impl.portal.FileChooser=termfilechooser
+  '';
 }
